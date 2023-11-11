a9eb6b331c4a75c6ba4f8e5f0aec090a7684846a
minkyung00 <mksin00@naver.com>
2023-11-11 21:34:56 +0900

    style: 기존 파일에 eslint fix 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
-import "./globals.css";
+import './globals.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="kr">
       <body>{children}</body>
