35b85832c1e8817bef397ab3222dcf38b276dc6f
hjy0951 <hjy0951@gmail.com>
2023-11-23 23:06:46 +0900

    feat: 서버에서 token 발급 후 사용자 email 접근

---
+++ src/app/login/oauth2/code/google/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+
+import { api } from '@/apis';
+
+export default function GoogleLogin() {
+  const [email, setEmail] = useState<string>('');
+  const searchParams = useSearchParams();
+  const token = searchParams.get('token');
+  // TODO: query string으로 JWT Token을 받아서 쿠키에 저장하는 로직이 필요합니다.
+
+  useEffect(() => {
+    try {
+      api.get('/my').then((response) => {
+        setEmail(response.data.email);
+        console.log(response.data.email);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }, [token]);
+
+  return <div>current user email: {email}</div>;
+}
