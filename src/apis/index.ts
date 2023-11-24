0b561187dce05d7833fb6aa36dec432a21aadd68
hjy0951 <hjy0951@gmail.com>
2023-11-24 13:09:04 +0900

    chore: feedback 반영

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,7 +1,7 @@
 import HttpClient from './httpClient';
 
 // TODO: API 서버 배포 후 설정 필요
-const API_URL = 'https://8d84-118-219-132-159.ngrok-free.app';
+const API_URL = '';
 
 export const api = new HttpClient({
   baseURL: API_URL,
