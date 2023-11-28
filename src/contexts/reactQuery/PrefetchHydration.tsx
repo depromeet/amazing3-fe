0ecfd226a4f71c733dfa764eaa830ec3ccfbc91e
minkyung00 <newminkyung@gmail.com>
2023-11-28 21:26:05 +0900

    chore: type -> interface로 변경

--- src/contexts/reactQuery/PrefetchHydration.tsx
+++ src/contexts/reactQuery/PrefetchHydration.tsx
@@ -1,10 +1,10 @@
 import { cache, type PropsWithChildren } from 'react';
 import { dehydrate, HydrationBoundary, QueryClient, type QueryFunction, type QueryKey } from '@tanstack/react-query';
 
-type FetchQueryOptions = {
+interface FetchQueryOptions {
   queryKey: QueryKey;
   queryFn: QueryFunction;
-};
+}
 
 const PrefetchHydration = async ({ queryKey, queryFn, children }: PropsWithChildren<FetchQueryOptions>) => {
   const getQueryClient = cache(() => new QueryClient());
