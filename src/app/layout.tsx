fdd6ccf2c23993d715559ec5d899a34ae570a02d
Doeunnkimm <doeunnkimm@gmail.com>
2023-11-29 15:24:23 +0900

    chore: default layout size 지정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,14 @@ import Providers from '@/contexts/Providers';
 import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const DEFAULT_LAYOUT_SIZE = 'max-w-[520px] min-h-[100dvh]';
+
   return (
     <html lang="kr">
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          <div className={`w-full mx-auto ${DEFAULT_LAYOUT_SIZE}`}>{children}</div>
+        </Providers>
       </body>
     </html>
   );
