cda33bbbad7d6975ae7180b6891d44698f370158
Doeunnkimm <doeunnkimm@gmail.com>
2023-11-30 17:16:36 +0900

    chore: tailwind의 @layer로 기본 layout 지정하도록 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,11 @@ import Providers from '@/contexts/Providers';
 import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const DEFAULT_LAYOUT_SIZE = 'max-w-[520px] min-h-[100dvh]';
-
   return (
     <html lang="kr">
       <body>
         <Providers>
-          <div className={`w-full mx-auto ${DEFAULT_LAYOUT_SIZE}`}>{children}</div>
+          <div className="layout">{children}</div>
         </Providers>
       </body>
     </html>
