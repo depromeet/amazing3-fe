f95d5ae1d76da4114ab8ae84525bc7d3f1693300
Doeunnkimm <doeunnkimm@gmail.com>
2023-12-01 22:23:20 +0900

    chore: 사용하지 않는 type 제거

--- src/features/auth/types/store.ts
+++ src/features/auth/types/store.ts
@@ -1,22 +1,4 @@
 /**
- * Auth 관련 상태 업데이트 actions
- */
-export interface AuthActionsProps {
-  /**
-   * 로그인 되어있는지 여부를 설정하는 함수
-   */
-  setIsSignedIn: (value: boolean) => void;
-  /**
-   * 토큰이 필요한지 여부를 설정하는 함수
-   */
-  setIsTokenRequired: (value: boolean) => void;
-  /**
-   * 토큰 관련 요청이 진행 중인지 여부를 설정하는 함수
-   */
-  setIsRequesting: (value: boolean) => void;
-}
-
-/**
  * Auth 관련 jotai state
  */
 export interface AuthStateProps {
