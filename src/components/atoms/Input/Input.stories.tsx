b8560e77ca78c7f633ce480c6a9b76bbceacbd8c
deepbig <hongsuk.ryu@silcroadsoft.com>
2023-12-03 22:38:03 +0900

    feat: input atom 개발 완료, button atom 기본 설정 변경

---
+++ src/components/atoms/Input/Input.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Input from './Input';
+
+const meta: Meta<typeof Input> = {
+  title: 'components/atoms/input',
+  component: Input,
+  argTypes: {
+    placeholder: {
+      control: {
+        type: 'text',
+      },
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    onChange: { action: 'changed' },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Input>;
+
+export const TextInput: Story = {
+  args: {
+    type: 'text',
+    placeholder: 'Text Input',
+  },
+};
+
+export const DateInput: Story = {
+  args: {
+    type: 'date',
+  },
+};
--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -1,11 +1,11 @@
-import { forwardRef } from 'react';
+import * as React from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 /**
  * TODO: 디자인 시스템에 맞게 수정
  */
 const buttonVariants = cva(
-  'inline-flex items-center justify-center word-break:keep-all text-white disabled:pointer-events-nonedisabled:pointer-events-none disabled:opacity-50',
+  'inline-flex h-10 items-center justify-center word-break:keep-all text-white disabled:pointer-events-nonedisabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       intent: {
@@ -19,19 +19,18 @@ const buttonVariants = cva(
           'bg-green-700 hover:bg-green-800 focus:bg-green-800 disabled:bg-gray-40',
         danger:
           'bg-red-700 hover:bg-red-800 focus:bg-red-800 disabled:bg-gray-40',
-        warning: 'bg-yellow-700 hover:bg-yellow-800 focus:bg-yellow-800',
       },
       size: {
-        xl: 'h-12 w-full rounded-lg px-[2.5rem] py-3',
-        lg: 'h-12 w-[16.25rem] rounded-md px-[2.5rem] py-3',
-        md: 'h-12 w-[12.5rem] rounded-md px-[2.5rem] py-3',
-        sm: 'h-12 w-[10.25rem] rounded-md px-[2.5rem] py-3',
-        xs: 'h-6 w-[315px] rounded-lg px-[2.5rem] py-3',
+        xl: 'w-full rounded-lg px-[2.5rem] py-3',
+        lg: 'w-[16.25rem] rounded-md px-[2.5rem] py-3',
+        md: 'w-[12.5rem] rounded-md px-[2.5rem] py-3',
+        sm: 'w-[10.25rem] rounded-md px-[2.5rem] py-3',
+        xs: 'w-[5px] rounded-lg px-[2.5rem] py-3',
       },
     },
     defaultVariants: {
       intent: 'primary',
-      size: 'lg',
+      size: 'xl',
     },
   },
 );
@@ -40,7 +39,7 @@ interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {}
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, intent, size, ...props }, ref) => {
     return (
       <button
