249fe03216ffab75c58d09fb7c4110ab7313833f
minkyung00 <newminkyung@gmail.com>
2023-12-04 23:50:47 +0900

    feat: 목표 날짜 설정 페이지 추가

---
+++ src/app/create/date/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useFormContext } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+
+const CreateDatePage = () => {
+  const router = useRouter();
+  const { register } = useFormContext();
+
+  const handleClickNextButton = () => {
+    router.push('/create/tag');
+  };
+
+  return (
+    <>
+      <input {...register('date')} type="number" placeholder="나이" />
+      <button onClick={handleClickNextButton}>다음</button>
+    </>
+  );
+};
+
+export default CreateDatePage;
