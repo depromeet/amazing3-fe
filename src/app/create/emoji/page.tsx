2bcdad74bae50fbb8dc62752fa9fdff3b02edd1c
minkyung00 <newminkyung@gmail.com>
2023-12-04 23:52:14 +0900

    feat: 이모지 선택 페이지 추가

---
+++ src/app/create/emoji/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useFormContext } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+
+import type { TargetFormValues } from '@/features/target/types';
+
+const CreateEmojiPage = () => {
+  const router = useRouter();
+  const { register } = useFormContext<TargetFormValues>();
+
+  const handleClickNextButton = () => {
+    router.push('/create/subtask');
+  };
+
+  return (
+    <>
+      <input {...register('emoji')} type="text" placeholder="이모지" />
+      <button onClick={handleClickNextButton}>다음</button>
+    </>
+  );
+};
+
+export default CreateEmojiPage;
