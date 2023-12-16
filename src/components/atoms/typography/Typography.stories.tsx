db1b855abff23ea68ceafa21756fbac39ab71e5c
Doeunnkimm <doeunnkimm@gmail.com>
2023-12-16 23:54:00 +0900

    chore: 디자인시스템 바탕으로 스타일 및 스토리 추가

--- src/components/atoms/typography/Typography.stories.tsx
+++ src/components/atoms/typography/Typography.stories.tsx
@@ -12,12 +12,6 @@ const meta: Meta<typeof Typography> = {
         options: ['heading2', 'heading3', 'body4', 'body5', 'subLabel2'],
       },
     },
-    textColor: {
-      control: {
-        type: 'select',
-        options: ['white', 'gray4', 'gray5', 'gray7', 'black'],
-      },
-    },
   },
 };
 
@@ -25,10 +19,9 @@ export default meta;
 
 type Story = StoryObj<typeof Typography>;
 
-export const Heading2Typography: Story = {
+export const Heading1: Story = {
   args: {
-    type: 'heading2',
-    textColor: 'black',
+    type: 'heading1',
     children: (
       <>
         Hello world!
@@ -39,34 +32,76 @@ export const Heading2Typography: Story = {
   },
 };
 
-export const Heading3Typography: Story = {
+export const Title1: Story = {
   args: {
-    type: 'heading3',
-    textColor: 'gray7',
-    children: 'Hello world!',
+    type: 'title1',
+    children: (
+      <>
+        Hello world!
+        <br />
+        We are amzaing3
+      </>
+    ),
   },
 };
 
-export const Body4Typography: Story = {
+export const Title3: Story = {
   args: {
-    type: 'body4',
-    textColor: 'gray5',
-    children: 'Hello world!',
+    type: 'title3',
+    children: (
+      <>
+        Hello world!
+        <br />
+        We are amzaing3
+      </>
+    ),
   },
 };
 
-export const Body5Typography: Story = {
+export const Title4: Story = {
   args: {
-    type: 'body5',
-    textColor: 'gray4',
-    children: 'Hello world!',
+    type: 'title4',
+    children: (
+      <>
+        Hello world!
+        <br />
+        We are amzaing3
+      </>
+    ),
   },
 };
 
-export const SubLabel2Typography: Story = {
+export const Body1: Story = {
+  args: {
+    type: 'body1',
+    children: 'We are amzaing3',
+  },
+};
+
+export const Body3: Story = {
+  args: {
+    type: 'body3',
+    children: 'We are amzaing3',
+  },
+};
+
+export const SubLabel1: Story = {
+  args: {
+    type: 'subLabel1',
+    children: 'We are amzaing3',
+  },
+};
+
+export const SubLabel2: Story = {
   args: {
     type: 'subLabel2',
-    textColor: 'gray4',
-    children: 'Hello world!',
+    children: 'We are amzaing3',
+  },
+};
+
+export const Caption1: Story = {
+  args: {
+    type: 'caption1',
+    children: 'We are amzaing3',
   },
 };
