bb4302d1fffe526fb391a18a1606c43a72a020b7
hjy0951 <hjy0951@gmail.com>
2023-12-18 02:28:24 +0900

    feat: 생년월일 입력 페이지에 ContentWrapper 적용

--- src/app/member/new/age/page.tsx
+++ src/app/member/new/age/page.tsx
@@ -1,24 +1,19 @@
-'use client';
-
-import React from 'react';
-
-import { Button, Input, Typography } from '@/components';
+import { Button, ContentWrapper, Input } from '@/components';
 
 /**
  * TODO: 추후 디자인 적용 필요
  */
 const AgeInputPage = () => {
+  const title = '반가워요, 닉네임님!\n생년월일을 입력해 주세요.';
+  const description = 'beta에서는 생년월일을 수정할 수 없어요.';
+
   return (
-    <div className="pt-10 flex flex-col gap-4">
-      <Typography type="heading2">
-        반가워요, OO님! <br /> 생년월일을 알려주세요.
-      </Typography>
-      <Typography type="subLabel2">
-        beta에서는 나이를 수정할 수 없어요. <br /> 신중하게 입력해 주세요.
-      </Typography>
-      <Input type="date" />
-      <Button type="button">완료</Button>
-    </div>
+    <>
+      <ContentWrapper title={title} description={description}>
+        <Input type="date" />
+        <Button type="button">완료</Button>
+      </ContentWrapper>
+    </>
   );
 };
 
