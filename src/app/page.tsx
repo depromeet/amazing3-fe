df15f8415f9978e42bacbf5b495346b2ad2444e5
hjy0951 <hjy0951@gmail.com>
2023-12-17 14:06:24 +0900

    feat: 스플래쉬 로그인 화면 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
-'use client';
+import Link from 'next/link';
+
+import { Typography } from '@/components';
+import { GoogleLoginButton } from '@/features/auth/components';
 
 export default function Home() {
-  const onClickButton = () => {
-    window.location.href = 'https://8d84-118-219-132-159.ngrok-free.app/oauth2/authorization/google';
-  };
-
   return (
-    <div>
-      <button type="button" onClick={onClickButton}>
-        google로 로그인
-      </button>
+    <div className="h-screen flex flex-col gap-2xs justify-center items-center bg-gradient1">
+      <Typography className="bg-clip-text bg-gradient4 text-transparent text-center" type="heading1">
+        내가 직접 그리는 <br /> 나의 인생지도
+      </Typography>
+      <div className="w-[300px] h-[250px] flex justify-center items-center bg-white">
+        <span>logo</span>
+      </div>
+      <GoogleLoginButton />
+      <Typography className="text-center text-gray-40" type="body3">
+        회원가입 시{' '}
+        <Link className=" underline" href="/">
+          서비스 이용약관
+        </Link>
+        과 <br />{' '}
+        <Link className=" underline" href="/">
+          개인정보 수집 및 이용
+        </Link>
+        에 동의하게 됩니다.
+      </Typography>
     </div>
   );
 }
