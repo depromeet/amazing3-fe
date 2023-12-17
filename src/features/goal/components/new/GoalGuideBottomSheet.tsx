057167f26da7122ffd389dd451e01d00982d0be6
deepbig <34956359+deepbig@users.noreply.github.com>
2023-12-17 20:05:38 +0900

    feat: GoalGuideBottomSheet의 handleClick 함수 추가

--- src/features/goal/components/new/GoalGuideBottomSheet.tsx
+++ src/features/goal/components/new/GoalGuideBottomSheet.tsx
@@ -1,5 +1,17 @@
-import { BottomSheet } from '@/components/atoms/bottomSheet';
-import { Button } from '@/components/atoms/button';
+import { useEffect, useState } from 'react';
+import * as Tabs from '@radix-ui/react-tabs';
+
+import {
+  educationalItems,
+  emotionalItems,
+  financialItems,
+  physicalItems,
+  relationalItems,
+  vocationalItems,
+} from '@/assets/goalGuides';
+import { SelectableCardList } from '@/components';
+import { BottomSheet, Button, Typography } from '@/components/atoms';
+import type { TabsProps } from '@/components/molecules/tabs';
 
 interface GoalGuideBottomSheetProps {
   open: boolean;
@@ -7,36 +19,72 @@ interface GoalGuideBottomSheetProps {
 }
 
 const GoalGuideBottomSheet = ({ open, onClose }: GoalGuideBottomSheetProps) => {
+  const [title, setTitle] = useState('');
+  const [selectedCardContent, setSelectedCardContent] = useState<string | React.ReactNode>('');
+  const [goalGuide, setGoalGuide] = useState<TabsProps>(); // GoalGuide is a state variable that stores the goal guide data
+
+  useEffect(() => {
+    setGoalGuide({
+      items: [
+        { label: '직업', content: <SelectableCardList items={vocationalItems} onClick={handleCardClick} /> },
+        { label: '경제력', content: <SelectableCardList items={financialItems} onClick={handleCardClick} /> },
+        { label: '배움', content: <SelectableCardList items={educationalItems} onClick={handleCardClick} /> },
+        { label: '신체/건강', content: <SelectableCardList items={physicalItems} onClick={handleCardClick} /> },
+        { label: '감정/정신', content: <SelectableCardList items={emotionalItems} onClick={handleCardClick} /> },
+        { label: '인간관계', content: <SelectableCardList items={relationalItems} onClick={handleCardClick} /> },
+      ],
+    });
+  }, []);
+
+  const handleCardClick = (itemContent: string | React.ReactNode) => {
+    console.log(itemContent);
+    setSelectedCardContent(itemContent); // Store the selected card's content in state
+  };
+
   const handleApply = () => {
-    // TODO: 선택된 목표 추천 가이드를 form('title')에 업데이트.
     onClose();
   };
 
   return (
-    <div className="flex items-center justify-center">
+    <Tabs.Root defaultValue="tab1">
       <BottomSheet
         open={open}
         onDismiss={onClose}
-        HeaderComponent={<Header />}
+        HeaderComponent={goalGuide && <Header items={goalGuide.items} />}
         FooterComponent={<Footer onDone={handleApply} />}
       >
-        <>
-          <p className="text-xl font-bold flex items-center justify-between mt-2mb-2">TODO</p>
-          <ul>
-            <li>목표 추천 가이드 리스트로 생성</li>
-            <li>리스트 선택 후 적용 버튼 클릭 시, form에 값 업데이트</li>
-            <li>Header, Footer, Body를 atom으로 변경</li>
-          </ul>
-        </>
+        {goalGuide && <Content items={goalGuide.items} />}
       </BottomSheet>
-    </div>
+    </Tabs.Root>
   );
 };
 
-/**
- * TODO: Typography 컴포넌트 반영 후 Header, footer 수정
- */
-const Header = () => <p className="text-xl font-bold flex items-center justify-between mb-2">문답 가이드</p>;
+const Header = ({ items }: TabsProps) => (
+  <>
+    <Typography type="heading3">목표 가이드</Typography>
+    <Tabs.List className="flex border-b border-gray-20">
+      {items.map((item, index) => (
+        <Tabs.Trigger
+          key={index}
+          className="px-5xs h-[45px] flex-grow items-center justify-center text-gray-40 hover:text-purple-80 data-[state=active]:text-purple-80 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative"
+          value={`tab${index + 1}`}
+        >
+          <Typography type="subLabel1">{item.label}</Typography>
+        </Tabs.Trigger>
+      ))}
+    </Tabs.List>
+  </>
+);
+
+const Content = ({ items }: TabsProps) => (
+  <>
+    {items.map((item, index) => (
+      <Tabs.Content key={index} className="p-2xs" value={`tab${index + 1}`}>
+        {item.content}
+      </Tabs.Content>
+    ))}
+  </>
+);
 
 const Footer = ({ onDone }: { onDone: VoidFunction }) => <Button onClick={onDone}>적용</Button>;
 
