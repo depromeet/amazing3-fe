4b7d647b13e25c59ac8089c25077018ed6883a7b
hjy0951 <hjy0951@gmail.com>
2023-12-18 15:07:58 +0900

    fix: 렌더링 이전에 버튼이 비활성화되도록 수정

--- src/features/member/components/new/NicknameInputForm.tsx
+++ src/features/member/components/new/NicknameInputForm.tsx
@@ -4,14 +4,17 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 import { Button, ContentWrapper, LimitedLengthInput } from '@/components';
+import { useIsMounted } from '@/hooks/useIsMounted';
 
 export const NicknameInputForm = () => {
   const title = '닉네임을 입력해 주세요.';
   const description = 'beta에서는 닉네임을 수정할 수 없어요.';
   const maxInputLength = 10;
   const placeholder = '닉네임';
-  const [isEmpty, setIsEmpty] = useState(false);
+
+  const [isEmpty, setIsEmpty] = useState<boolean>(false);
   const router = useRouter();
+  const isMounted = useIsMounted();
 
   const handleClickNextButton = () => {
     router.push('/member/new/birthday');
@@ -20,7 +23,7 @@ export const NicknameInputForm = () => {
   return (
     <ContentWrapper title={title} description={description}>
       <LimitedLengthInput maxLength={maxInputLength} placeholder={placeholder} setIsEmpty={setIsEmpty} />
-      <Button type="button" disabled={isEmpty} onClick={handleClickNextButton}>
+      <Button type="button" disabled={isMounted ? isEmpty : true} onClick={handleClickNextButton}>
         다음
       </Button>
     </ContentWrapper>
