b65f8f4eaecc2879be85d079a88c05998648102f
hjy0951 <hjy0951@gmail.com>
2023-12-18 14:54:31 +0900

    feat: 페이지에 AgeInputForm 적용

--- src/app/member/new/age/page.tsx
+++ src/app/member/new/age/page.tsx
@@ -1,20 +1,10 @@
-import { Button, ContentWrapper, Input } from '@/components';
+import { AgeInputForm } from '@/features/member/components/new';
 
 /**
  * TODO: 추후 디자인 적용 필요
  */
 const AgeInputPage = () => {
-  const title = '반가워요, 닉네임님!\n생년월일을 입력해 주세요.';
-  const description = 'beta에서는 생년월일을 수정할 수 없어요.';
-
-  return (
-    <>
-      <ContentWrapper title={title} description={description}>
-        <Input type="date" />
-        <Button type="button">완료</Button>
-      </ContentWrapper>
-    </>
-  );
+  return <AgeInputForm />;
 };
 
 export default AgeInputPage;
