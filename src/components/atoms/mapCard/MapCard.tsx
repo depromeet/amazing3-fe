2f88b7b35a79194e65afc296896af9c284b6192e
Doeunnkimm <doeunnkimm@gmail.com>
2023-12-19 02:04:42 +0900

    feat: Image에 priority 및 스켈레톤 ui 적용

--- src/components/atoms/mapCard/MapCard.tsx
+++ src/components/atoms/mapCard/MapCard.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import VerticalBarIcon from '@/assets/icons/vertical-bar.svg';
+import { blueDataURL } from '@/constants';
 import type { MapCardGoalProps } from '@/features/home/types';
 
 import { Typography } from '../typography';
@@ -19,7 +20,15 @@ export const MapCard = ({ goal, position }: MapCardProps) => {
       href={{ pathname: `/goal/${id}` }}
       className={`absolute w-[130px] h-[130px] rounded-lg bg-white pt-[5px] pb-[6px] px-[16px] shadow-thumb cursor-pointer ${position.x} ${position.y}`}
     >
-      <Image src={stickerImage} width={100} height={100} alt="sticker" />
+      <Image
+        src={stickerImage}
+        width={100}
+        height={100}
+        alt="sticker"
+        priority
+        placeholder="blur"
+        blurDataURL={blueDataURL.mapCard}
+      />
       <div className="flex gap-[4px] justify-center items-center">
         <Typography type="title5" className="text-blue-50">
           {deadline}
---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export { blueDataURL } from './blurDataURL';
