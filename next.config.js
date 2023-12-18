68315b9ad432a2b87fb0c586160068303d327285
hjy0951 <hjy0951@gmail.com>
2023-12-19 04:51:49 +0900

    chore: svgr 설치 및 nextjs config 설정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
+  // 웹팩을 통해 svg 파일을 리액트 컴포넌트로 변환하는 설정
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
