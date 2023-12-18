59c22b9298a0a79c5f3fa748c775dbe1b1d5b7f0
Doeunnkimm <doeunnkimm@gmail.com>
2023-12-19 00:14:18 +0900

    refactor: MapCard props 변경대로 Story도 반영

--- src/components/atoms/mapCard/MapCard.stories.tsx
+++ src/components/atoms/mapCard/MapCard.stories.tsx
@@ -13,9 +13,12 @@ type Story = StoryObj<typeof MapCard>;
 
 export const Basic: Story = {
   args: {
-    stickerImage: 'https://github.com/depromeet/amazing3-fe/assets/112946860/b266a620-a349-4f70-8236-be1612028a97',
-    deadline: '2024.01',
-    tag: '학업',
+    goal: {
+      id: 1,
+      stickerImage: 'https://github.com/depromeet/amazing3-fe/assets/112946860/b266a620-a349-4f70-8236-be1612028a97',
+      deadline: '2024.01',
+      tag: '학업',
+    },
     position: ['top-[20px]', 'left-[20px]'],
   },
 };
