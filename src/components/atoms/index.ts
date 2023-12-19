9e40c85c302f0196c8fc8814e5ef0a706a1e860a
Doeunnkimm <doeunnkimm@gmail.com>
2023-12-19 23:29:36 +0900

    feat: Star 컴포넌트 추가

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -3,4 +3,5 @@ export { BottomSheet } from './bottomSheet';
 export { Button } from './button';
 export { Input } from './input';
 export { Span } from './span';
+export { Star } from './star';
 export { Typography } from './typography';
