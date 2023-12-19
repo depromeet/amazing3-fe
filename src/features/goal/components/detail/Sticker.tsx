9259bb5727bbb7e32ee1cabc1eb4e335ffc3cc3c
deepbig <34956359+deepbig@users.noreply.github.com>
2023-12-19 20:02:58 +0900

    feat: sticker에 jotai atom 적용

--- src/features/goal/components/detail/Sticker.tsx
+++ src/features/goal/components/detail/Sticker.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
+import { useAtomValue } from 'jotai';
 
 import Target from '@/assets/stickers/target-512x512.png';
+import { goalStickerAtom } from '@/features/goal/components/detail/atom';
 
+// TODO: api로 src url 받아오면 target image 삭제
 export const Sticker = () => {
-  return <Image className="absolute" alt="Target" src={Target} width={200} height={200} />;
+  const sticker = useAtomValue(goalStickerAtom);
+
+  return <Image className="absolute" alt="Target" src={Target || sticker} width={200} height={200} />;
 };
