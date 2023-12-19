626803e2fba1b8a3cb32e0d45fb00bedae78a8ac
Doeunnkimm <doeunnkimm@gmail.com>
2023-12-19 23:29:54 +0900

    chore: 홈화면 layout에 별 적용

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,9 +1,21 @@
 import type { PropsWithChildren } from 'react';
 
+import { Star } from '@/components';
+
 const HomeLayout = ({ children }: PropsWithChildren) => {
   return (
-    <main className="py-xl flex justify-center bg-gradient1">
-      <div className="w-[390px] relative px-xl">{children}</div>
+    <main className="flex justify-center bg-gradient1">
+      <div className="w-[390px] relative px-xl pt-xs">
+        <Star size={19} position={{ x: 'left-[171px]', y: 'top-[189px]' }} />
+        <Star size={15} position={{ x: 'right-[23px]', y: 'top-[168px]' }} color="blue" />
+        <Star size={15} position={{ x: 'left-[53px]', y: 'top-[368px]' }} />
+        <Star size={26} position={{ x: 'left-[116px]', y: 'top-[370px]' }} color="blue" />
+        <Star size={16} position={{ x: 'right-[41px]', y: 'top-[465px]' }} color="blue" />
+        <Star size={26} position={{ x: 'left-[123px]', y: 'top-[630px]' }} color="blue" />
+        <Star size={15} position={{ x: 'left-[24px]', y: 'top-[603px]' }} color="blue" />
+        <Star size={26} position={{ x: 'right-[48px]', y: 'top-[630px]' }} color="blue" />
+        {children}
+      </div>
     </main>
   );
 };
