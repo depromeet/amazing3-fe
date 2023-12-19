b2206815bfde5db5b5ee260bc2f6fdcc127ae86b
Doeunnkimm <doeunnkimm@gmail.com>
2023-12-19 15:57:09 +0900

    chore: 불필요한 스타일 부분 제거

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
  * TODO: 디자인 시스템에 맞게 수정
  */
 const buttonVariants = cva(
-  'inline-flex gap-[6px] h-10 items-center justify-center word-break:keep-all disabled:pointer-events-nonedisabled:pointer-events-none disabled:opacity-30 transition-colors duration-200 font-semibold',
+  'inline-flex gap-[6px] items-center justify-center word-break:keep-all disabled:pointer-events-nonedisabled:pointer-events-none disabled:opacity-30 transition-colors duration-200 font-semibold',
   {
     variants: {
       variant: {
