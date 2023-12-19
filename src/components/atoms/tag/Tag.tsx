dd3882aeb6e3ae1a3aae6a6dede25152ddd99d25
Doeunnkimm <doeunnkimm@gmail.com>
2023-12-20 03:55:53 +0900

    chore: 스타일 일부 추가

--- src/components/atoms/tag/Tag.tsx
+++ src/components/atoms/tag/Tag.tsx
@@ -8,14 +8,14 @@ interface TagProps {
 
 export const Tag = ({ isFocus = false, children }: PropsWithChildren<TagProps>) => {
   return (
-    <div
-      className={`flex justify-center items-center w-fit px-3xs py-4xs shadow-thumb rounded-xl ${
+    <label
+      className={`flex justify-center items-center w-fit px-3xs py-4xs shadow-thumb rounded-xl cursor-pointer ${
         isFocus && 'bg-blue-20'
       }`}
     >
       <Typography type="title3" className={`${!isFocus ? 'text-gray-40' : 'text-blue-50'}`}>
         {children}
       </Typography>
-    </div>
+    </label>
   );
 };
