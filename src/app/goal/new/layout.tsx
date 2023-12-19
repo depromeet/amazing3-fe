d772809ee09d84e594a10bde88bc7c58dcfa5e5f
Heojoooon <45158550+hjy0951@users.noreply.github.com>
2023-12-19 23:30:24 +0900

    feat: 목표 추가 페이지 배경 설정 (#27)
    * chore: 목표 추가 페이지에 사용할 배경 이미지 추가
    
    * design: 목표 추가 페이지 배경 적용
    
    * design: 아래 24px 여백 생성
    
    * design: 목표 추가 레이아웃 수정
    
    * fix: 목표 추가 페이지에 스크롤 생성되지 않도록 수정
    
    * fix: 구글 로고 아이콘 컴포넌트로 import
    
    * design: wrapping된 div 삭제
    
    * chore: 반디부디 캐릭터 png 교체
    
    * design: 반디부디 캐릭터 z-index 조정

--- src/app/goal/new/layout.tsx
+++ src/app/goal/new/layout.tsx
@@ -1,11 +1,31 @@
 'use client';
 
 import type { PropsWithChildren } from 'react';
+import Image from 'next/image';
 
+import BandiBoodi from '@/assets/images/bandi-boodi.png';
+import SpeechBubble from '@/assets/images/bg-speech-bubble.svg';
+import Stars from '@/assets/images/bg-stars.svg';
+import Wave from '@/assets/images/bg-wave.svg';
 import CreateGoalFormProvider from '@/features/goal/contexts/CreateGoalFormProvider';
 
 const Layout = ({ children }: PropsWithChildren) => {
-  return <CreateGoalFormProvider>{children}</CreateGoalFormProvider>;
+  return (
+    <CreateGoalFormProvider>
+      <div className="w-full h-screen bg-gradient1 relative">
+        <SpeechBubble className="absolute top-[-4%] w-full" />
+        <Stars className="absolute top-[28%] left-1/4 w-1/2" />
+        <Image
+          className="absolute top-[28%] left-1/3 w-1/3 z-[1]"
+          src={BandiBoodi}
+          alt="BandiBoodi Character"
+          priority
+        />
+        <Wave className="absolute top-[40%] w-full" />
+        <div className="absolute top-0 left-0 w-full h-[calc(100vh-24px)]">{children}</div>
+      </div>
+    </CreateGoalFormProvider>
+  );
 };
 
 export default Layout;
