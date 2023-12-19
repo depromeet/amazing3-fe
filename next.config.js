ca0df2008d5b52590397476e514e79b12e964baf
deepbig <34956359+deepbig@users.noreply.github.com>
2023-12-20 03:37:03 +0900

    feat: api 연동

--- next.config.js
+++ next.config.js
@@ -13,6 +13,14 @@ const nextConfig = {
 
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,8 +1,5 @@
 import HttpClient from './httpClient';
 
-// TODO: API 서버 배포 후 설정 필요
-const API_URL = '';
-
 export const api = new HttpClient({
-  baseURL: API_URL,
+  baseURL: '/api',
 });
