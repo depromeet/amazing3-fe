070045b95c9c6868a05d8ff591f7d3fc7a70a7da
hjy0951 <hjy0951@gmail.com>
2023-12-20 03:56:29 +0900

    feat: 목표 생성중 title 선택 후, date 선택시 화면에 표기

--- src/features/goal/components/new/DateForm.tsx
+++ src/features/goal/components/new/DateForm.tsx
@@ -11,16 +11,17 @@ import { DateInput } from './DateInput';
 import FormLayout from './FormLayout';
 
 export const DateForm = () => {
-  const { register, control } = useFormContext<GoalFormValues>();
+  const { register, getValues, control } = useFormContext<GoalFormValues>();
   const { field } = useController({ name: 'date', control });
   const { onChange, value } = field;
+  const { title } = getValues();
 
   return (
     <FormLayout
       header={<Typography className="text-gray-50 font-insungit text-center">header</Typography>}
       comment={
         <Typography type="title3" className="text-gray-50 font-insungit text-center">
-          목표 <br /> <Span type="blue50">언제</Span>까지 이루고 싶어?
+          {title} <br /> <Span type="blue50">언제</Span>까지 이루고 싶어?
         </Typography>
       }
       body={
