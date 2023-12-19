c46fb0eecc2ea4ab269b624a0b5b9ebaafb9f588
Doeunnkimm <doeunnkimm@gmail.com>
2023-12-20 04:13:57 +0900

    chore: HTMLLabelElement 타입 상속받도록

--- src/components/atoms/tag/Tag.tsx
+++ src/components/atoms/tag/Tag.tsx
@@ -1,17 +1,19 @@
-import type { PropsWithChildren } from 'react';
+import type { HTMLProps, PropsWithChildren } from 'react';
 
 import { Typography } from '../typography';
 
-interface TagProps {
+interface TagProps extends HTMLProps<HTMLLabelElement> {
   isFocus?: boolean;
 }
 
-export const Tag = ({ isFocus = false, children }: PropsWithChildren<TagProps>) => {
+export const Tag = ({ isFocus = false, children, ...props }: PropsWithChildren<TagProps>) => {
   return (
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
     <label
       className={`flex justify-center items-center w-fit px-3xs py-4xs shadow-thumb rounded-xl cursor-pointer ${
         isFocus && 'bg-blue-20'
       }`}
+      {...props}
     >
       <Typography type="title3" className={`${!isFocus ? 'text-gray-40' : 'text-blue-50'}`}>
         {children}
