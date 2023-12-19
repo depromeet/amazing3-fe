32a3b0143efa7250bbcc593f087498e1901173ef
Doeunnkimm <doeunnkimm@gmail.com>
2023-12-19 18:08:35 +0900

    feat: MapCardPositioner 컴포넌트 추가

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,4 +1,5 @@
 export { ContentWrapper } from './contentWrapper';
 export { LimitedLengthInput } from './limitedLengthInput';
+export { MapCardPositioner } from './mapCardPositioner';
 export { SelectableCardList } from './selectableCardList';
 export { Tabs } from './tabs';
