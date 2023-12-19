d958433cd4d6f10c96afc93553bcb9e3b451d199
deepbig <34956359+deepbig@users.noreply.github.com>
2023-12-20 03:37:03 +0900

    feat: api 연동

--- next.config.js
+++ next.config.js
@@ -16,6 +16,14 @@ const nextConfig = {
 
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- src/app/login/oauth2/code/google/page.tsx
+++ src/app/login/oauth2/code/google/page.tsx
@@ -12,11 +12,18 @@ const GoogleLogin = () => {
   // TODO: query string으로 JWT Token을 받아서 쿠키에 저장하는 로직이 필요합니다.
 
   useEffect(() => {
+    console.log('token: ', token);
     try {
-      api.get('/my').then((response: AxiosResponse) => {
-        setEmail(response.data.email);
-        console.log(response.data.email);
-      });
+      api
+        .get('/my', {
+          headers: {
+            Authorization: `Bearer ${token}`, // replace `yourToken` with your actual token
+          },
+        })
+        .then((response: AxiosResponse) => {
+          setEmail(response.data.email);
+          console.log(response.data.email);
+        });
     } catch (error) {
       console.error(error);
     }
