8aa4b7ff34d2f4272a1b015c1fa8f84bfb300829
hjy0951 <hjy0951@gmail.com>
2023-12-21 03:09:03 +0900

    feat: new goal FormHeader 구현

---
+++ src/features/goal/components/new/FormHeader.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+import BackIcon from '@/assets/icons/goal/back-icon.svg';
+import CloseIcon from '@/assets/icons/goal/close-icon.svg';
+
+import { NEW_GOAL_FORM_ORDERS } from '../../constants';
+
+interface FormHeaderProps {
+  formNumber: number;
+}
+
+const FormHeader = ({ formNumber }: FormHeaderProps) => {
+  const totalPages = Object.keys(NEW_GOAL_FORM_ORDERS).length;
+  const router = useRouter();
+  const handleClickBackButton = () => {
+    router.back();
+  };
+
+  return (
+    <div className="flex justify-between items-center">
+      <button onClick={handleClickBackButton}>
+        <BackIcon />
+      </button>
+      <div className="w-[30%]">
+        <div className="relative w-[100%] h-[6px] rounded-lg bg-white">
+          <div
+            className={`absolute top-0 left-0 w-[${(formNumber / totalPages) * 100}%] h-[6px] rounded-lg bg-blue-30`}
+          />
+        </div>
+      </div>
+      <Link href={{ pathname: '/home' }}>
+        <CloseIcon />
+      </Link>
+    </div>
+  );
+};
+
+export default FormHeader;
---
+++ src/features/goal/constants/goal.ts
@@ -0,0 +1,7 @@
+export const NEW_GOAL_FORM_ORDERS = {
+  goal: 1,
+  date: 2,
+  tag: 3,
+  sticker: 4,
+  more: 5,
+};
