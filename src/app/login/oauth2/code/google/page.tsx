89d8609a5cfb33c17e75dee43e762cf698dd1ec0
Doeunnkimm <doeunnkimm@gmail.com>
2023-12-20 22:55:32 +0900

    chore: 불필요한 부분 제거

--- src/app/login/oauth2/code/google/page.tsx
+++ src/app/login/oauth2/code/google/page.tsx
@@ -2,29 +2,32 @@
 
 import { useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useSetAtom } from 'jotai';
+import { useAtom, useSetAtom } from 'jotai';
 import Cookies from 'js-cookie';
 
+import { isLoginAtom } from '@/features/auth/atom';
 import { memberAtom } from '@/features/member/atoms';
-import { useAuth } from '@/hooks/useAuth';
+import { useGetMemberData } from '@/hooks/reactQuery/auth';
 
 const GoogleLogin = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
-  const setMemberData = useSetAtom(memberAtom);
 
-  const { isLogin, memberData } = useAuth();
+  const setMemberData = useSetAtom(memberAtom);
+  const [isLogin, setIsLogin] = useAtom(isLoginAtom);
+
+  const { data: memberData } = useGetMemberData();
 
   useEffect(() => {
     if (token) {
       Cookies.set('accessToken', token, { secure: process.env.NODE_ENV !== 'development', expires: 7 });
     }
+    setIsLogin(true);
   }, [token]);
 
   useEffect(() => {
     setMemberData({ ...memberData });
-
     if (isLogin) {
       router.push(memberData?.username ? '/home' : '/member/new/nickname');
     }
