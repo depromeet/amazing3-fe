ee45ce09aa9ad55c2c3a0b7d09063fb648312d7c
minkyung00 <newminkyung@gmail.com>
2023-12-20 21:37:59 +0900

    feat: ShareButton props 추가

--- src/features/home/components/shareButton/ShareButton.tsx
+++ src/features/home/components/shareButton/ShareButton.tsx
@@ -1,12 +1,14 @@
+import type { ButtonHTMLAttributes } from 'react';
+
 import ShareIcon from '@/assets/icons/share.svg';
 
 /**
  * TODO 1: 디자인 시스템 반영된 Button으로 대체하기
  * TODO 2: 클릭 이벤트 연결하기
  */
-export const ShareButton = () => {
+export const ShareButton = (props: ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
-    <button className="w-[60px] h-[60px] p-2xs flex justify-center items-center bg-blue-10 rounded-lg">
+    <button className="w-[60px] h-[60px] p-2xs flex justify-center items-center bg-blue-10 rounded-lg" {...props}>
       <ShareIcon />
     </button>
   );
