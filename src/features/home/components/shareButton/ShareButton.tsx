b2fc211bb82de4575ae97d9364d924462d6fd8dc
minkyung00 <newminkyung@gmail.com>
2023-12-21 21:45:33 +0900

    fix: 공유 버튼 아이콘 변경 및 스피너 추가

--- src/features/home/components/shareButton/ShareButton.tsx
+++ src/features/home/components/shareButton/ShareButton.tsx
@@ -1,15 +1,21 @@
 import type { ButtonHTMLAttributes } from 'react';
 
-import ShareIcon from '@/assets/icons/share.svg';
+import DownloadIcon from '@/assets/icons/download.svg';
+import { Spinner } from '@/components/atoms/spinner';
 
 /**
  * TODO 1: 디자인 시스템 반영된 Button으로 대체하기
  * TODO 2: 클릭 이벤트 연결하기
  */
-export const ShareButton = (props: ButtonHTMLAttributes<HTMLButtonElement>) => {
+
+interface ShareButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isLoading: boolean;
+}
+
+export const ShareButton = ({ isLoading, ...props }: ShareButtonProps) => {
   return (
     <button className="w-[60px] h-[60px] p-2xs flex justify-center items-center bg-blue-10 rounded-lg" {...props}>
-      <ShareIcon />
+      {isLoading ? <Spinner /> : <DownloadIcon />}
     </button>
   );
 };
