53aaf7058ec315b9401fc64adbfa798e3979aece
Hongsuk Ryu <34956359+deepbig@users.noreply.github.com>
2023-12-22 19:32:31 +0900

    chore: vercel/analytics 적용 (#57)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react';
+
 import Providers from '@/contexts/Providers';
 
 import './globals.css';
@@ -5,9 +7,13 @@ import './globals.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="kr">
+      <head>
+        <title>반디부디: 별이 되고 싶은 반디부디의 인생지도</title>
+      </head>
       <body>
         <Providers>
           <div className="layout">{children}</div>
+          <Analytics />
         </Providers>
       </body>
     </html>
