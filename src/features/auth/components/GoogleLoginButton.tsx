51db422157df01dc5d7be4a662fdc0481bd493ae
Hongsuk Ryu <34956359+deepbig@users.noreply.github.com>
2023-12-22 14:27:18 +0900

    chore: 연동 이슈 문제 해결 (#53)

--- src/features/auth/components/GoogleLoginButton.tsx
+++ src/features/auth/components/GoogleLoginButton.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components';
 
 export const GoogleLoginButton = () => {
   return (
-    <Link href={{ pathname: '/api/oauth2/authorization/google' }}>
+    <Link href={{ pathname: `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/google` }}>
       <Button>
         <GoogleIcon width={24} alt="google-logo" />
         Google 로그인
