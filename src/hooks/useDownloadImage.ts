8dc881fcfd95f16b6cf292de2add56071d674382
minkyung <newminkyung@gmail.com>
2023-12-28 22:05:31 +0900

    fix: ios에서 이미지 다운로드가 제대로 되지 않는 버그 해결 (#73)
    * feat: 이미지 공유 기능 추가
    
    * feat: ios 식별할 수 있는 유틸 함수 추가
    
    * chore: modern-screenshot 패키지 설치
    
    * chore: html-to-image 패키지 제거
    
    * fix: html-to-image -> modern-screenshot 변경
    
    * fix: ios인 경우, 이미지 공유 기능으로 변경

--- src/hooks/useDownloadImage.ts
+++ src/hooks/useDownloadImage.ts
@@ -1,5 +1,8 @@
 import { type RefObject, useState } from 'react';
-import { toJpeg } from 'html-to-image';
+import { domToJpeg } from 'modern-screenshot';
+
+import { shareImage } from '@/utils/image';
+import { isIos } from '@/utils/userAgent';
 
 import { backgroundImage } from '../../styles/theme';
 
@@ -23,19 +26,20 @@ export const useDownloadImage = (imageRef: RefObject<HTMLElement>) => {
     try {
       setIsDownloading(true);
 
-      const imageUrl = await toJpeg(image, {
-        includeQueryParams: true,
+      const imageUrl = await domToJpeg(image, {
         style: {
           backgroundImage: backgroundImage.gradient1,
-          paddingTop: '24px',
-          paddingBottom: '24px',
         },
         height: 700,
+        scale: 4,
       });
 
-      // FIXME: 다운로드 되는 이미지 파일 이름 수정 필요
       const IMAGE_FILE_NAME = '별이되고_싶은_반디부디의_인생지도';
-      downloadFile(imageUrl, IMAGE_FILE_NAME);
+      if (isIos()) {
+        shareImage(imageUrl, IMAGE_FILE_NAME);
+      } else {
+        downloadFile(imageUrl, IMAGE_FILE_NAME);
+      }
     } catch (error) {
       // TODO: 이미지 다운로드 실패 시, 추가 에러 처리 필요
       console.error(error);
