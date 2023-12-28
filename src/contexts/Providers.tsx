b5476e8907caecc77a7d1368e128dd4d0832971f
minkyung <newminkyung@gmail.com>
2023-12-28 22:41:15 +0900

    fix: 카카오톡 인앱 브라우저로 접근 시, 외부 브라우저로 이동하도록 수정 (#74)
    * feat: 카카오 인앱 브라우저 식별하는 유틸 함수 추가
    
    * feat: 카카오톡 인앱 브라우저로 접속 시, 외부 브라우저로 이동시키는 기능 구현

--- src/contexts/Providers.tsx
+++ src/contexts/Providers.tsx
@@ -3,9 +3,13 @@
 import type { PropsWithChildren } from 'react';
 import { OverlayProvider } from '@toss/use-overlay';
 
+import { useOpenExternalBrowser } from '@/hooks/useOpenExternalBrowser';
+
 import QueryClientProvider from './reactQuery/QueryClientProvider';
 
 const Providers = ({ children }: PropsWithChildren) => {
+  useOpenExternalBrowser();
+
   return (
     <QueryClientProvider>
       <OverlayProvider>{children}</OverlayProvider>
