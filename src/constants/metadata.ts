e2d830e27c88cfb324b80471c87f87c27fa85ed1
김도은 <doeunnkimm@gmail.com>
2023-12-28 21:42:19 +0900

    bug: og tag에서 불필요한 부분 제거 및 ogImage 명시 (#72)
    * bug: metadata 및 manifest 재설정 및 추가
    
    * chore: safari의 경우 webmanifest의 icon 컬렉션을 무시 → 별도로 명시
    
    * chore: static 하게 webmanifest로 명시
    
    * chore: manifest 401 에러 해결
    
    * chore: 적용되지 않거나 불필요한 코드 제거
    
    * chore: iOS 기기에서 풀스크린 모드로 실행할 수 있게 하는 메타 태그 추가
    
    * chore: manifest.webmanifest 파일 적용 시도
    
    * chore: 적용되지 않는 코드 제거
    
    * chore: ogImage size 변경(→1200x630)
    
    * chore: 네이밍 컨벤션 맞게 수정
    
    * refactor: icon 관련 link 태그 대신 metadata 객체 요소로 icon 설정
    
    * chore: ogImage 용량 줄이기
    
    * fix: apple-touch-icon만 명시적으로 link 태그 통해 입력

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -4,6 +4,8 @@ export const META = {
   description:
     '이루고 싶은 목표를 지도에 남겨보세요. 목표와 관련된 다양한 스티커를 붙여 나만의 지도를 만들고 공유할 수 있어요.',
   keyword: ['반디부디', 'bandiboodi', '인생지도', '신년계획', '계획', '목표설정', '목표달성', '자기계발'],
-  domain_URL: 'https://www.bandiboodi.com/',
-  google_verification: process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION,
+  url: 'https://www.bandiboodi.com/',
+  googleVerification: process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION,
+  iconImage: '/icon.ico',
+  ogImage: '/opengraph-image.png',
 } as const;
