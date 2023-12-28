fbffffacf5e0be57f02f3700e9cbd76cc826b965
minkyung00 <newminkyung@gmail.com>
2023-12-28 15:14:56 +0900

    fix: ios인 경우, 이미지 공유 기능으로 변경

--- src/hooks/useDownloadImage.ts
+++ src/hooks/useDownloadImage.ts
@@ -1,6 +1,9 @@
 import { type RefObject, useState } from 'react';
 import { domToJpeg } from 'modern-screenshot';
 
+import { shareImage } from '@/utils/image';
+import { isIos } from '@/utils/userAgent';
+
 import { backgroundImage } from '../../styles/theme';
 
 const downloadFile = (url: string, filename: string) => {
@@ -32,7 +35,11 @@ export const useDownloadImage = (imageRef: RefObject<HTMLElement>) => {
       });
 
       const IMAGE_FILE_NAME = '별이되고_싶은_반디부디의_인생지도';
-      downloadFile(imageUrl, IMAGE_FILE_NAME);
+      if (isIos()) {
+        shareImage(imageUrl, IMAGE_FILE_NAME);
+      } else {
+        downloadFile(imageUrl, IMAGE_FILE_NAME);
+      }
     } catch (error) {
       // TODO: 이미지 다운로드 실패 시, 추가 에러 처리 필요
       console.error(error);
