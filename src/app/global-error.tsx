4f6bf56759775a782f13bda4f8239b89901d364a
minkyung00 <newminkyung@gmail.com>
2023-12-29 22:16:23 +0900

    feat: 모든 에러 발생 시, sentry 에러 캡쳐 추가

---
+++ src/app/global-error.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useEffect } from 'react';
+import NextError from 'next/error';
+import * as Sentry from '@sentry/nextjs';
+
+const GlobalError = ({ error }: { error: Error & { digest?: string } }) => {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'development') return;
+
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <html lang="ko-KR">
+      <body>
+        {/* This is the default Next.js error component but it doesn't allow omitting the statusCode property yet. */}
+        {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+        <NextError statusCode={undefined as any} />
+      </body>
+    </html>
+  );
+};
+
+export default GlobalError;
