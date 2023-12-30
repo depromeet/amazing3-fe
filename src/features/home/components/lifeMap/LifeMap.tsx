6d2c0217a3a202ad0c3d03c9ee0fb809b8931092
minkyung00 <newminkyung@gmail.com>
2023-12-30 22:36:14 +0900

    feat: 홈페이지 공유 바텀 시트 추가

--- src/features/home/components/lifeMap/LifeMap.tsx
+++ src/features/home/components/lifeMap/LifeMap.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import Link from 'next/link';
 import { SwiperSlide } from 'swiper/react';
 
@@ -8,24 +8,29 @@ import StarBg from '@/app/home/startBg';
 import { Avatar, Button, ContentWrapper } from '@/components';
 import { useGetMemberData } from '@/hooks/reactQuery/auth';
 import { useGetGoals } from '@/hooks/reactQuery/goal';
-import { useDownloadImage } from '@/hooks/useDownloadImage';
 
 import { GOAL_COUNT_PER_PAGE } from '../../constants';
 import { makeHomeDescription } from '../../utils/makeHomeDescription';
 import { MapCardPositioner } from '../mapCardPositioner';
 import { partitionArrayWithSmallerFirstGroup } from '../mapCardPositioner/MapCardPositioner.utils';
 import { MapSwiper } from '../mapSwiper';
+import { ShareBottomSheet } from '../shareBottomSheet';
 import { ShareButton } from '../shareButton';
 
 export const LifeMap = () => {
+  const [openShareBottomSheet, setOpenShareBottomSheet] = useState(false);
+
   const { data: memberData } = useGetMemberData();
   const { data: goalsData } = useGetGoals();
 
   const downloadSectionRef = useRef<HTMLElement>(null);
-  const { isDownloading, onDownloadImage } = useDownloadImage(downloadSectionRef);
 
   const participatedGoalsArray = partitionArrayWithSmallerFirstGroup(GOAL_COUNT_PER_PAGE, goalsData?.goals);
   const LAST_PAGE = participatedGoalsArray.length;
+
+  const handleOpenShareBottomSheet = (isOpen: boolean) => () => {
+    setOpenShareBottomSheet(isOpen);
+  };
 
   return (
     <div className="w-full h-[100dvh] flex flex-col items-center justify-between pb-xs">
@@ -80,7 +85,12 @@ export const LifeMap = () => {
         </ContentWrapper>
       </div>
       <div className="flex gap-5xs px-xs pt-5xs mt-[18px] w-full">
-        <ShareButton isLoading={isDownloading} onClick={onDownloadImage} />
+        <ShareButton onClick={handleOpenShareBottomSheet(true)} />
+        <ShareBottomSheet
+          ref={downloadSectionRef}
+          open={openShareBottomSheet}
+          onClose={handleOpenShareBottomSheet(false)}
+        />
         <Link href={{ pathname: '/goal/new/goal' }} className="w-full">
           <Button>목표 추가하기</Button>
         </Link>
