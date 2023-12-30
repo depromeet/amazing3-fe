0c9812c470fba861359d5b8f1dfa452b1a62c6cb
minkyung00 <newminkyung@gmail.com>
2023-12-30 22:36:52 +0900

    chore: 공유버튼 아이콘 수정

--- src/features/home/components/shareButton/ShareButton.tsx
+++ src/features/home/components/shareButton/ShareButton.tsx
@@ -1,21 +1,16 @@
 import type { ButtonHTMLAttributes } from 'react';
 
-import DownloadIcon from '@/assets/icons/download.svg';
-import { Spinner } from '@/components/atoms/spinner';
+import ShareIcon from '@/assets/icons/share.svg';
 
 /**
  * TODO 1: 디자인 시스템 반영된 Button으로 대체하기
  * TODO 2: 클릭 이벤트 연결하기
  */
 
-interface ShareButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  isLoading: boolean;
-}
-
-export const ShareButton = ({ isLoading, ...props }: ShareButtonProps) => {
+export const ShareButton = ({ ...props }: ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
     <button className="w-[60px] h-[60px] p-2xs flex justify-center items-center bg-blue-10 rounded-lg" {...props}>
-      {isLoading ? <Spinner /> : <DownloadIcon />}
+      <ShareIcon />
     </button>
   );
 };
