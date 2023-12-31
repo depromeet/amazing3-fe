c0070972f3943931835f6a566494c013ace3e877
hjy0951 <hjy0951@gmail.com>
2023-12-31 21:44:12 +0900

    refactor: 중복되는 코드 삭제

--- src/features/goal/components/new/DateInput.tsx
+++ src/features/goal/components/new/DateInput.tsx
@@ -19,6 +19,10 @@ export const DateInput = ({ maxLength, textAlign = 'start', additionalText = '',
   const [formattedValue, setFormattedValue] = useState<string>('');
   const placeholder = (maxLength === MAX_DATE_LENGTH_UNTIL_MONTH ? 'YYYY.MM' : 'YYYY.MM.DD') + additionalText;
 
+  const makeFormattedDate = (strArr: string[], separator: string) => {
+    return strArr.join(separator);
+  };
+
   const isValidate = (year: string, month: string, day: string) => {
     const date = year + '-' + month + '-' + day;
     return !isNaN(Date.parse(date));
@@ -30,17 +34,21 @@ export const DateInput = ({ maxLength, textAlign = 'start', additionalText = '',
     let year, month, day;
 
     if (inputValue.length > 4 && inputValue.length <= 6) {
+      year = inputValue.slice(0, 4);
       month = inputValue.slice(4, 6);
       month = +month > 12 ? '12' : +month === 0 ? '0' : month;
-      formatted = inputValue.slice(0, 4) + '.' + month;
+      formatted = makeFormattedDate([year, month], '.');
     } else if (inputValue.length > 6) {
+      year = inputValue.slice(0, 4);
+      month = inputValue.slice(4, 6);
+      day = inputValue.slice(6, 8);
+
       if (inputValue.length < 8) {
-        formatted = inputValue.slice(0, 4) + '.' + inputValue.slice(4, 6) + '.' + inputValue.slice(6, 8);
+        formatted = makeFormattedDate([year, month, day], '.');
       } else {
-        year = inputValue.slice(0, 4);
-        month = inputValue.slice(4, 6);
-        day = inputValue.slice(6, 8);
-        formatted = isValidate(year, month, day) ? year + '.' + month + '.' + day : year + '.' + month;
+        formatted = isValidate(year, month, day)
+          ? makeFormattedDate([year, month, day], '.')
+          : makeFormattedDate([year, month], '.');
       }
     }
     setFormattedValue(formatted);
