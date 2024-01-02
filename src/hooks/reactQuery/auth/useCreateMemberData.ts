bc7cb87bc87126016c84c95f26164eefe1c21dd3
Hongsuk Ryu <34956359+deepbig@users.noreply.github.com>
2024-01-03 00:24:30 +0900

    bug: 사용자 상태에 따른 onboarding 시작하기 기능 분리 및 metadata 수정 (#83)
    * bug: onboarding에서 사용자 nickname 설정 후 db에서 nickname 값을 가져올 수 있도록 수정
    
    * bug: onboarding에서 사용자의 상태에 따른 시작하기 동작 분리
    
    * chore: onboarding 화면에 맞는 metadata 적용
    
    * chore: 상속받지 못하는 필드 추가

--- src/hooks/reactQuery/auth/useCreateMemberData.ts
+++ src/hooks/reactQuery/auth/useCreateMemberData.ts
@@ -1,10 +1,13 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { api } from '@/apis';
 import type { NewMemberFormValues as MemberRequest } from '@/features/member/types';
 
 export const useCreateMemberData = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (data: MemberRequest) => api.put('/my', data),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['memberData'] }),
   });
 };
