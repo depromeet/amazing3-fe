1f59ebdb632f17e66f142a5da7b9dac0bdee74ba
hjy0951 <hjy0951@gmail.com>
2024-01-04 15:06:36 +0900

    refactor: input 검증 함수 네이밍 변경

--- src/features/member/components/new/BirthInputForm.tsx
+++ src/features/member/components/new/BirthInputForm.tsx
@@ -17,7 +17,7 @@ export const BirthInputForm = () => {
   const { onChange, value } = field;
   const { nickname } = getValues();
 
-  const isValidInput = () => (value ? value.length !== MAX_DATE_LENGTH_UNTIL_DAY : true);
+  const isInvalidInput = () => (value ? value.length !== MAX_DATE_LENGTH_UNTIL_DAY : true);
 
   return (
     <FormLayout
@@ -30,7 +30,7 @@ export const BirthInputForm = () => {
           <div {...register('birth')}>
             <DateInput maxLength={MAX_DATE_LENGTH_UNTIL_DAY} onChange={onChange} />
           </div>
-          <Button type="submit" disabled={isValidInput()}>
+          <Button type="submit" disabled={isInvalidInput()}>
             완료
           </Button>
         </div>
