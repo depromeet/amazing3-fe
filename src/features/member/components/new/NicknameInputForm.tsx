7b3beab4fbcc3df3e1521d36ac9182625e052f04
deepbig <34956359+deepbig@users.noreply.github.com>
2024-01-06 06:41:56 +0900

    bug: 로그인 성공한 사용자가 onboarding에서 refetch 전에 페이지 이동하는 문제 해결
    기존 onboarding 페이지에서 사용자 정보 refetch하는 로직이 정상동작하지 않아, nickname 페이지에서 사용자 정보를 확인하는 로직으로 변경했습니다. 이에 따라 불필요한 코드 또한 정리했습니다.

--- src/features/member/components/new/NicknameInputForm.tsx
+++ src/features/member/components/new/NicknameInputForm.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { useController, useFormContext } from 'react-hook-form';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 import SmileIcon from '@/assets/icons/smile-iocn.svg';
 import { Button, LimitedLengthInput } from '@/components';
 import { MAX_NICKNAME_LENGTH } from '@/constants';
+import { useGetMemberData } from '@/hooks/reactQuery/auth/useGetMemberData';
 import { useIsMounted } from '@/hooks/useIsMounted';
 
 import type { NewMemberFormValues } from '../../types';
@@ -17,27 +20,43 @@ export const NicknameInputForm = () => {
   const { register, control } = useFormContext<NewMemberFormValues>();
   const { field } = useController({ name: 'nickname', control });
   const { onChange, value } = field;
+  const { data: memberData } = useGetMemberData();
+  const router = useRouter();
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    if (memberData) {
+      const nickname = memberData?.nickname;
+      if (nickname) {
+        router.push('/home');
+      } else {
+        setIsReady(true);
+      }
+    }
+  }, [memberData, router]);
 
   const isInvalidInput = () => (value ? value.length === 0 : true);
 
   return (
-    <FormLayout
-      icon={<SmileIcon width={40} height={40} />}
-      title="닉네임을 입력해 주세요."
-      description="beta에서는 닉네임을 수정할 수 없어요."
-    >
-      <div className="mt-xs flex flex-col grow w-full">
-        <div className="h-full flex flex-col justify-between">
-          <div {...register('nickname')}>
-            <LimitedLengthInput maxLength={MAX_NICKNAME_LENGTH} placeholder="닉네임" onChange={onChange} />
+    isReady && (
+      <FormLayout
+        icon={<SmileIcon width={40} height={40} />}
+        title="닉네임을 입력해 주세요."
+        description="beta에서는 닉네임을 수정할 수 없어요."
+      >
+        <div className="mt-xs flex flex-col grow w-full">
+          <div className="h-full flex flex-col justify-between">
+            <div {...register('nickname')}>
+              <LimitedLengthInput maxLength={MAX_NICKNAME_LENGTH} placeholder="닉네임" onChange={onChange} />
+            </div>
+            <Link href="/member/new/birth">
+              <Button type="button" disabled={!isMounted || isInvalidInput()}>
+                다음
+              </Button>
+            </Link>
           </div>
-          <Link href="/member/new/birth">
-            <Button type="button" disabled={!isMounted || isInvalidInput()}>
-              다음
-            </Button>
-          </Link>
         </div>
-      </div>
-    </FormLayout>
+      </FormLayout>
+    )
   );
 };
--- src/features/onboarding/components/onboardingBody/OnboardingBody.tsx
+++ src/features/onboarding/components/onboardingBody/OnboardingBody.tsx
@@ -1,16 +1,13 @@
 'use client';
 
-import { useEffect } from 'react';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { SwiperSlide } from 'swiper/react';
 
 import MapSticker from '@/assets/stickers/map_sticker.png';
 import TargetSticker from '@/assets/stickers/target_sticker.png';
 import WritingSticker from '@/assets/stickers/writing_sticker.png';
 import { Button } from '@/components';
-import { Spinner } from '@/components/atoms/spinner';
-import { useGetMemberData } from '@/hooks/reactQuery/auth';
 
 import { OnboardingLayout } from '../onboardingLayout';
 import type { OnboardingLayoutProps } from '../onboardingLayout/onboardingLayout';
@@ -50,24 +47,6 @@ const ONBOARDING_VALUES: OnboardingLayoutProps[] = [
 ];
 
 export const OnboardingBody = () => {
-  const router = useRouter();
-  const { data: memberData, refetch, isFetching } = useGetMemberData({ enabled: false });
-
-  useEffect(() => {
-    if (memberData) {
-      const nickname = memberData?.nickname;
-      if (nickname) {
-        router.push('/home');
-      } else {
-        router.push('/member/new/nickname');
-      }
-    }
-  }, [memberData, router]);
-
-  const handleClick = () => {
-    refetch();
-  };
-
   return (
     <div className="w-full h-full px-xs flex flex-col">
       <div className="h-full">
@@ -80,9 +59,9 @@ export const OnboardingBody = () => {
         </OnboardingSwiper>
       </div>
       <div className="h-full flex flex-col-reverse">
-        <Button onClick={handleClick} disabled={isFetching} className="flex-grow-1">
-          {isFetching ? <Spinner /> : '시작하기'}
-        </Button>
+        <Link href={{ pathname: '/member/new/nickname' }}>
+          <Button className="flex-grow-1">시작하기</Button>
+        </Link>
       </div>
     </div>
   );
