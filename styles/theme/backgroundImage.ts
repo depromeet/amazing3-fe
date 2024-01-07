f9d03765229bc2de35336bbc16e58cb5a90dcb7d
Doeunnkimm <doeunnkimm@gmail.com>
2024-01-07 21:52:16 +0900

    chore: 그라데이션 스타일 추가

--- styles/theme/backgroundImage.ts
+++ styles/theme/backgroundImage.ts
@@ -3,4 +3,5 @@ export const backgroundImage = {
   gradient2: 'linear-gradient(90deg, rgba(252,243,255,1) 0%, rgba(182,207,255,1) 100%)',
   gradient3: 'linear-gradient(90deg, rgba(140,104,255,1) 0%, rgba(113,203,255,1) 100%)',
   gradient4: 'linear-gradient(90deg, rgba(64,94,206,1) 0%, rgba(95,121,214,1) 50%, rgba(127,108,244,1) 100%)',
+  gradient5: 'linear-gradient(295deg, rgba(252, 243, 255, 1) -2.54%, rgba(234, 241, 255, 1) 100.8%)',
 };
