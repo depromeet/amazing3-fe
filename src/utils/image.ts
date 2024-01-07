82ba0dbcd4676b81737a2a31acd72057b20053e7
minkyung00 <newminkyung@gmail.com>
2024-01-07 15:56:54 +0900

    refactor: 이미지 url을 반환하는 함수 분리

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,3 +1,9 @@
+import { domToJpeg } from 'modern-screenshot';
+
+import { DOWNLOAD_IMAGE_STYLE } from '@/constants/image';
+
+import { backgroundImage } from '../../styles/theme';
+
 export const shareImage = async (imageUrl: string, fileName: string) => {
   // TODO: 추후에 에러 처리 변경
   if (!navigator.canShare) {
@@ -23,4 +29,28 @@ export const shareImage = async (imageUrl: string, fileName: string) => {
   }
 
   await navigator.share(shareData);
+};
+
+export const getImageUrl = async (image: HTMLElement, currentPage: number) => {
+  const { WIDTH, HEIGHT, PADDING } = DOWNLOAD_IMAGE_STYLE;
+
+  const imageUrl = await domToJpeg(image, {
+    style: {
+      backgroundImage: backgroundImage.gradient1,
+      paddingTop: `${PADDING}px`,
+    },
+    height: HEIGHT,
+    scale: 4,
+    onCloneNode: (node) => {
+      const swiperWrapper = (node as HTMLElement).querySelector<HTMLElement>('.swiper-wrapper');
+      const swiperPagination = (node as HTMLElement).querySelector<HTMLElement>('.swiper-pagination');
+
+      if (!swiperWrapper || !swiperPagination) return;
+
+      swiperPagination.style.display = 'none';
+      swiperWrapper.style.transform = `translate3d(-${currentPage * WIDTH}px, 0, 0)`;
+    },
+  });
+
+  return imageUrl;
 };
