aad0d561c39423a6e58697095b9d9611c17cb99f
hjy0951 <hjy0951@gmail.com>
2024-01-08 17:41:43 +0900

    feat: 유저 프로필 이미지 및 정보 추가

--- src/features/my/components/userProfile/UserProfile.tsx
+++ src/features/my/components/userProfile/UserProfile.tsx
@@ -1,9 +1,21 @@
+import { Avatar } from '@/components';
+
+import { USER_PROFILE_IMAGE_SIZE } from '../../constants/profile';
+
 import ProfileCard from './ProfileCard';
+import UserInfo from './UserInfo';
 
 const UserProfile = () => {
+  const nickname = '일이삼사오육칠팔구십';
+  const birth = '1999.08.23';
+  const username = 'bandiboodi';
+
   // TODO: API 연결로 데이터 받아오기
+
   return (
     <div className="flex flex-col items-center justify-center">
+      <Avatar size={USER_PROFILE_IMAGE_SIZE} />
+      <UserInfo nickname={nickname} birth={birth} username={username} />
       <ProfileCard days={131} totalGoals={4} />
     </div>
   );
---
+++ src/features/my/constants/profile.ts
@@ -0,0 +1,1 @@
+export const USER_PROFILE_IMAGE_SIZE = 151;
