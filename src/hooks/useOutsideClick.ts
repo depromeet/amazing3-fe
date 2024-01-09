d314ccc285e50e534ff8cdc79c5362496e169b8f
Doeunnkimm <doeunnkimm@gmail.com>
2024-01-10 00:09:28 +0900

    feat: useOutsideClick 커스텀훅 추가

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,17 @@
+import type { RefObject } from 'react';
+import { useEffect } from 'react';
+
+export const useOutsideClick = (ref: RefObject<HTMLElement>, callback: VoidFunction) => {
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref, callback]);
+};
