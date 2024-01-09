913ac7fac05add0a048818962c13209ea12fdb23
hjy0951 <hjy0951@gmail.com>
2024-01-09 16:39:21 +0900

    feat: 로그아웃 기능 추가

--- src/features/my/components/logoutBottomSheet/Footer.tsx
+++ src/features/my/components/logoutBottomSheet/Footer.tsx
@@ -1,20 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
+
 import { Button } from '@/components/atoms';
+import { Spinner } from '@/components/atoms/spinner';
 
 interface FooterProps {
   onClose: VoidFunction;
 }
 
 const Footer = ({ onClose }: FooterProps) => {
+  const router = useRouter();
+  const [isLogout, setIsLogout] = useState<boolean>(false);
+
   const handleClickLogout = () => {
-    // TODO: 쿠키의 토큰 삭제
+    Cookies.remove('accessToken');
+    setIsLogout(true);
   };
+
+  useEffect(() => {
+    if (!isLogout) return;
+    router.push('/');
+  }, [isLogout, router]);
+
   return (
     <div className="flex gap-5xs mb-5xs">
       <Button variant="tertiary" onClick={onClose}>
         취소
       </Button>
       <Button variant="blue" onClick={handleClickLogout}>
-        로그아웃
+        {isLogout ? <Spinner /> : '로그아웃'}
       </Button>
     </div>
   );
