5df994dfe3b63752f1308796a2dd347aee54b8c5
hjy0951 <hjy0951@gmail.com>
2024-01-10 00:44:09 +0900

    refactor: 피드백 반영

--- src/features/my/components/MyPageHeader.tsx
+++ src/features/my/components/MyPageHeader.tsx
@@ -7,15 +7,14 @@ import { Typography } from '@/components';
 
 const MypageHeader = () => {
   const router = useRouter();
+
   const handleClickBackButton = () => {
     router.back();
   };
 
   return (
     <div className="flex justify-between items-center">
-      <button onClick={handleClickBackButton}>
-        <BackIcon />
-      </button>
+      <BackIcon onClick={handleClickBackButton} />
       <Typography type="title1">마이페이지</Typography>
       {/* TODO: 수정 버튼 클릭시 수정페이지로 이동 */}
       <Link href="/my">
--- src/features/my/components/logoutBottomSheet/Footer.tsx
+++ src/features/my/components/logoutBottomSheet/Footer.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Cookies from 'js-cookie';
 
 import { Button } from '@/components/atoms';
-import { Spinner } from '@/components/atoms/spinner';
 
 interface FooterProps {
   onClose: VoidFunction;
@@ -11,17 +9,11 @@ interface FooterProps {
 
 const Footer = ({ onClose }: FooterProps) => {
   const router = useRouter();
-  const [isLogout, setIsLogout] = useState<boolean>(false);
 
   const handleClickLogout = () => {
     Cookies.remove('accessToken');
-    setIsLogout(true);
-  };
-
-  useEffect(() => {
-    if (!isLogout) return;
     router.push('/');
-  }, [isLogout, router]);
+  };
 
   return (
     <div className="flex gap-5xs mb-5xs">
@@ -29,7 +21,7 @@ const Footer = ({ onClose }: FooterProps) => {
         취소
       </Button>
       <Button variant="blue" onClick={handleClickLogout}>
-        {isLogout ? <Spinner /> : '로그아웃'}
+        로그아웃
       </Button>
     </div>
   );
