e7b4a370718628bf4eff56345ec6962be532861a
hjy0951 <hjy0951@gmail.com>
2024-01-11 00:49:46 +0900

    refactor: UserProfile 속성 추가

--- src/features/my/components/MyPageLayout.tsx
+++ src/features/my/components/MyPageLayout.tsx
@@ -6,14 +6,19 @@ import MyPageBody from './MyPageBody';
 import MypageHeader from './MyPageHeader';
 
 export const MyPageLayout = () => {
+  const nickname = '일이삼사오육칠팔구십';
+  const birth = '1999.08.23';
+  const username = 'bandiboodi';
+  const isPublic = true;
+
   // TODO: API 연결로 데이터 받아오기
 
   return (
     <div className="pt-5xs px-xs h-full flex flex-col">
       <MypageHeader />
-      <UserProfile />
+      <UserProfile nickname={nickname} username={username} birth={birth} />
       <MyPageBody />
-      <LifeMapPrivacySetting isPublic={true} />
+      <LifeMapPrivacySetting isPublic={isPublic} />
     </div>
   );
 };
--- src/features/my/components/userProfile/UserProfile.tsx
+++ src/features/my/components/userProfile/UserProfile.tsx
@@ -5,11 +5,13 @@ import { USER_PROFILE_IMAGE_SIZE } from '../../constants';
 import ProfileCard from './ProfileCard';
 import UserInfo from './UserInfo';
 
-const UserProfile = () => {
-  const nickname = '일이삼사오육칠팔구십';
-  const birth = '1999.08.23';
-  const username = 'bandiboodi';
+interface UserProfileProps {
+  nickname: string;
+  username: string;
+  birth: string;
+}
 
+const UserProfile = ({ nickname, username, birth }: UserProfileProps) => {
   return (
     <div className="mt-md flex flex-col gap-lg items-center justify-center">
       <div className="flex flex-col gap-5xs items-center">
