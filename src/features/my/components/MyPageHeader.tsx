f536f258ef7bb0374909291f47eb499edb15a661
hjy0951 <hjy0951@gmail.com>
2024-01-11 01:18:56 +0900

    refactor: 피드백 반영

--- src/features/my/components/MyPageHeader.tsx
+++ src/features/my/components/MyPageHeader.tsx
@@ -1,20 +1,15 @@
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 
 import EditIcon from '@/assets/icons/edit-icon.svg';
 import BackIcon from '@/assets/icons/goal/back-icon.svg';
 import { Typography } from '@/components';
 
 const MypageHeader = () => {
-  const router = useRouter();
-
-  const handleClickBackButton = () => {
-    router.back();
-  };
-
   return (
     <div className="flex justify-between items-center">
-      <BackIcon onClick={handleClickBackButton} />
+      <Link href="/home">
+        <BackIcon />
+      </Link>
       <Typography type="title1">마이페이지</Typography>
       {/* TODO: 수정 버튼 클릭시 수정페이지로 이동 */}
       <Link href="/my">
