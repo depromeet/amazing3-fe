d6ba1a96e3e3690a4d9a5fa4a52282fba7c6df0f
hjy0951 <hjy0951@gmail.com>
2024-01-11 20:01:06 +0900

    feat: API 명세에 맞게 MemberProps 속성 추가

--- src/features/member/types/form.ts
+++ src/features/member/types/form.ts
@@ -10,4 +10,25 @@ export interface MemberProps {
   nickname: string;
   birth: string;
   image: string;
+  createdAt: string;
+  lifeMap: { isPublic: boolean };
+  goal: { count: number };
 }
+
+/*
+  {
+    "id": 4,
+    "email": "amazingdpm@gmail.com",
+    "username": "BANDIBOODI-4",
+    "nickname": "김도무룩",
+    "birth": "2024-01-11",
+    "image": "https://lh3.googleusercontent.com/a/ACg8ocKNpvNZtTxiKysd68nIjE3KCgp5d_h979LBglkTJsuP=s96-c",
+    "createdAt": "2024-01-11T00:23:34",
+    "lifeMap": {
+        "isPublic": true
+    },
+    "goal": {
+        "count": 20
+    }
+  }
+  */
