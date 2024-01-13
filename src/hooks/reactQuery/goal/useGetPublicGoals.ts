c5873e4a635e2f868c6456e496b9492a05ff4b31
minkyung00 <newminkyung@gmail.com>
2024-01-13 10:37:39 +0900

    feat: username으로 인생지도 조회하는 api 훅 추가

---
+++ src/hooks/reactQuery/goal/useGetPublicGoals.ts
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { api } from '@/apis';
+
+import type { GoalResponse } from './useGetGoals';
+
+type GoalRequestParams = {
+  username: string;
+};
+
+export const useGetPublicGoals = ({ username }: GoalRequestParams) => {
+  return useQuery<GoalResponse>({
+    queryKey: ['life-map', username],
+    queryFn: () => api.get<GoalResponse>(`/open/life-map/${username}`),
+    enabled: !!username,
+  });
+};
