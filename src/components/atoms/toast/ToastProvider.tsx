171f30220deb064c1c13d0f895ce146e68723053
Doeunnkimm <doeunnkimm@gmail.com>
2024-01-14 13:26:07 +0900

    chore: Portal만 import하도록 import문 수정

--- src/components/atoms/toast/ToastProvider.tsx
+++ src/components/atoms/toast/ToastProvider.tsx
@@ -1,4 +1,4 @@
-import * as Portal from '@radix-ui/react-portal';
+import { Portal } from '@radix-ui/react-portal';
 import { useAtomValue } from 'jotai';
 
 import { Toast } from './Toast';
@@ -9,12 +9,12 @@ export const ToastProvider = () => {
   const { position } = useAtomValue(toastOptionAtom);
 
   return (
-    <Portal.Portal>
+    <Portal>
       <div className={`fixed ${position} left-1/2 transform translate-x-[-50%]`}>
         {toasts.map((toast) => (
           <Toast key={toast.id} {...toast} />
         ))}
       </div>
-    </Portal.Portal>
+    </Portal>
   );
 };
