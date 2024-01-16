56f07e039d4e433d3851c65b791fbf5f0994ced4
김도은 <doeunnkimm@gmail.com>
2024-01-16 13:02:33 +0900

    bug: Portal에서 hydration 이슈 해결 (#121)

--- src/components/atoms/toast/ToastProvider.tsx
+++ src/components/atoms/toast/ToastProvider.tsx
@@ -1,20 +1,25 @@
 import { Portal } from '@radix-ui/react-portal';
 import { useAtomValue } from 'jotai';
 
+import { useIsMounted } from '@/hooks';
+
 import { Toast } from './Toast';
 import { toastOptionAtom, toastsAtom } from './Toast.atom';
 
 export const ToastProvider = () => {
+  const isMounted = useIsMounted();
   const toasts = useAtomValue(toastsAtom);
   const { position } = useAtomValue(toastOptionAtom);
 
   return (
-    <Portal>
-      <div className={`fixed ${position} left-0 right-0 flex flex-col items-center z-50`}>
-        {toasts.map((toast) => (
-          <Toast key={toast.id} {...toast} />
-        ))}
-      </div>
-    </Portal>
+    isMounted && (
+      <Portal>
+        <div className={`fixed ${position} left-0 right-0 flex flex-col items-center z-50`}>
+          {toasts.map((toast) => (
+            <Toast key={toast.id} {...toast} />
+          ))}
+        </div>
+      </Portal>
+    )
   );
 };
