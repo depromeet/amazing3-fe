d208aa1913c045f72a6c05fc3f7057bcb9399e3f
minkyung <newminkyung@gmail.com>
2024-01-17 01:40:09 +0900

    feat: 인생지도 공유 url 화면 추가 (#116)
    * feat: username으로 인생지도 조회하는 api 훅 추가
    
    * feat: username dynamic routes 추가
    
    * feat: username으로 인생지도 조회하는 api 적용
    
    * feat: 인생지도 공개여부에 따른 에러 이미지 추가
    
    * feat: ErrorPageLayout에 403 에러 추가 및 리셋버튼 분리
    
    * feat: 지도 페이지에 에러바운더리 추가
    
    * fix: /home/{username}만 접근 가능하도록 수정
    
    * feat: username으로 인생지도 공유할 때 에러 던지도록 옵션 추가
    
    * fix: /home/username으로만 접근 가능하도록 라우팅 수정
    
    * chore: 이미지 사이즈 축소
    
    * refactor: br태그 -> 이스케이프 시퀀스로 수정
    
    * refactor: route segement 수정
    
    * fix: isLoginAtom의 초기 값을 accessToken 쿠키 값으로 변경
    
    * fix: 로그인한 경우에만 데이터 조회하도록 수정
    
    * feat: 로그인 + 다른 회원의 인생지도를 조회했을 때만 publicGoals을 조회하도록 수정
    
    * fix: 다른 회원의 인생지도를 조회한 경우, 해당 회원의 닉네임과 이미지가 보이도록 수정
    
    * refactor: 하단 버튼을 제외한 컴포넌트인 LifeMapContent로 분라
    
    * feat: PrivateLifeMap 컴포넌트 구현
    
    * feat: PublicLifeMap 컴포넌트 구현
    
    * feat: 조회한 지도가 내 공유인지에 따라 PrivateMap, PublicMap 조건부 렌더링
    
    * refactor: 중복되는 태그 분리
    
    * fix: public 지도에 접근할 시, 클릭 못하도록 수정
    
    * fix: home -> home/username으로 변경

---
+++ src/hooks/reactQuery/goal/useGetPublicGoals.ts
@@ -0,0 +1,26 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+
+import { api } from '@/apis';
+import type { MemberProps } from '@/features/member/types';
+
+import type { GoalResponse } from './useGetGoals';
+
+type GoalRequestParams = {
+  username: string;
+};
+
+type PublicGoalResponse = GoalResponse & { user: Pick<MemberProps, 'nickname' | 'image'> };
+
+export const useGetPublicGoals = ({ username }: GoalRequestParams) => {
+  const queryClient = useQueryClient();
+  const memberData = queryClient.getQueryData<MemberProps>(['memberData']);
+
+  const isDifferentUser = username !== memberData?.username;
+
+  return useQuery<PublicGoalResponse>({
+    queryKey: ['life-map', username],
+    queryFn: () => api.get<GoalResponse>(`/open/life-map/${username}`),
+    enabled: isDifferentUser,
+    throwOnError: true,
+  });
+};
