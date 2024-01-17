b627a38061d9c7419ee1de11f492173db4f21acd
minkyung00 <newminkyung@gmail.com>
2024-01-17 16:57:00 +0900

    fix: ios 이미지 다운로드 시, 공유 기능 -> 파일 다운로드로 수정

--- src/hooks/useDownloadImage.ts
+++ src/hooks/useDownloadImage.ts
@@ -2,8 +2,7 @@ import { type RefObject, useState } from 'react';
 import { domToJpeg } from 'modern-screenshot';
 
 import { GOAL_COUNT_PER_PAGE } from '@/features/home/constants';
-import { downloadFile, shareImage } from '@/utils/image';
-import { isIos } from '@/utils/userAgent';
+import { downloadFile } from '@/utils/image';
 
 import { backgroundImage } from '../../styles/theme';
 
@@ -61,8 +60,8 @@ export const useDownloadImage = ({ type, imageRef }: DownloadImageOption) => {
       const imageUrl = await domToJpeg(image, imageOption);
 
       const IMAGE_FILE_NAME = '별이되고_싶은_반디부디의_인생지도';
-      if (isIos()) shareImage(imageUrl, IMAGE_FILE_NAME);
-      else downloadFile(imageUrl, IMAGE_FILE_NAME);
+
+      downloadFile(imageUrl, IMAGE_FILE_NAME);
     } catch (error) {
       // TODO: 이미지 다운로드 실패 시, 추가 에러 처리 필요
       console.error(error);
