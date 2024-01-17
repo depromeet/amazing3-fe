35028fd68674c3ef68a206cc4e8a16f4fc41ee8b
deepbig <34956359+deepbig@users.noreply.github.com>
2024-01-18 01:07:26 +0900

    bug: 처음 로그인 시 token 값 확인 못하는 문제 해결

--- src/app/oauth2/token/page.tsx
+++ src/app/oauth2/token/page.tsx
@@ -2,29 +2,34 @@
 
 import { useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { useAtom } from 'jotai';
 import Cookies from 'js-cookie';
 
+import { isLoginAtom } from '@/features/auth/atom';
 import { useGetMemberData } from '@/hooks/reactQuery/auth';
 
 const OAuthTokenPage = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
+  const [isLogin, setIsLogin] = useAtom(isLoginAtom);
 
+  // const { data: memberData } = useGetMemberData({ enabled: true });
   const { data: memberData } = useGetMemberData();
 
   // TODO: useGetMemberData 가져오는 로직이 항상 token 저장 이후에 실행되도록 수정 필요.
   useEffect(() => {
     if (token) {
       Cookies.set('accessToken', token, { secure: process.env.NODE_ENV !== 'development', expires: 7 });
+      setIsLogin(true);
     }
-  }, [token]);
+  }, [setIsLogin, token]);
 
   useEffect(() => {
-    if (memberData) {
+    if (isLogin && memberData) {
       router.push(memberData?.nickname ? `/home/${memberData?.username}` : '/onboarding');
     }
-  }, [memberData, router]);
+  }, [memberData, router, isLogin]);
 
   return <></>;
 };
