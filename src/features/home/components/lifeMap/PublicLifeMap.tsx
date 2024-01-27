f7942fff1194e5a27aa197ad800e5a5a3e4a5060
minkyung00 <newminkyung@gmail.com>
2024-01-28 02:57:15 +0900

    feat: 응원보내기 버튼 ui 추가

--- src/features/home/components/lifeMap/PublicLifeMap.tsx
+++ src/features/home/components/lifeMap/PublicLifeMap.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 
 import { Button } from '@/components/atoms';
+import LikeButton from '@/features/LifeMapView/LikeButton';
 import { useGetMemberData } from '@/hooks/reactQuery/auth';
 import { useGetPublicGoals } from '@/hooks/reactQuery/goal/useGetPublicGoals';
 
@@ -13,12 +14,17 @@ export const PublicLifeMap = ({ username }: { username: string }) => {
   const { data: memberData } = useGetMemberData();
   const myHomePath = memberData?.username ? `/home/${memberData.username}` : '/';
 
+  const handleClickLikeButton = () => {
+    // TODO: 응원 보내기 버튼 클릭 시 api 추가 예정
+  };
+
   return (
     <>
       <LifeMapContent isPublic goalsData={publicGoals} memberData={publicGoals?.user} />
-      <div className="px-xs pt-5xs mt-[18px] w-full">
+      <div className="flex gap-5xs  px-xs pt-5xs mt-[18px] w-full">
+        <LikeButton onClick={handleClickLikeButton} />
         <Link href={{ pathname: myHomePath }} className="w-full">
-          <Button>{memberData?.username ? '내 지도로 돌아가기' : '로그인하기'}</Button>
+          <Button>{memberData?.username ? '내 지도로 돌아가기' : '내 지도 만들기'}</Button>
         </Link>
       </div>
     </>
