0f132a5ed3008a6a6a8c76e8413c1e891088e139
Doeunnkimm <doeunnkimm@gmail.com>
2024-01-28 01:57:38 +0900

    chore: 세부목표 추가 완료 시 ToolTip 추가

--- src/features/goal/components/detail/SavedFooterButton.tsx
+++ src/features/goal/components/detail/SavedFooterButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-import { Button } from '@/components';
+import { Button, ToolTip } from '@/components';
 import { useGetMemberData } from '@/hooks/reactQuery/auth';
 
 interface SavedFooterButtonProps {
@@ -12,8 +12,13 @@ export const SavedFooterButton = ({ goalId }: SavedFooterButtonProps) => {
   const { data: memberData } = useGetMemberData();
 
   return (
-    <Link href={{ pathname: `/home/${memberData?.username}`, query: { id: goalId } }}>
-      <Button>홈으로 가기</Button>
-    </Link>
+    <div className="relative">
+      <div className="absolute w-full flex justify-center bottom-[90px]">
+        <ToolTip title="홈화면에서 세부목표를 정할 수 있어요" />
+      </div>
+      <Link href={{ pathname: `/home/${memberData?.username}`, query: { id: goalId } }}>
+        <Button>홈으로 가기</Button>
+      </Link>
+    </div>
   );
 };
