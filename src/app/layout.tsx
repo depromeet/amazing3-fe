bd57c692b9b30aff53e9a6cd383a338dfd4f36d3
Doeunnkimm <doeunnkimm@gmail.com>
2024-01-28 00:31:42 +0900

    chore: import문 정리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
-import type { Metadata } from 'next';
-import type { Viewport } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
