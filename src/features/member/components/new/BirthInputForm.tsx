1754f7090d1b34c812acad8d30ebd2367a8f082c
Doeunnkimm <doeunnkimm@gmail.com>
2024-01-27 13:31:29 +0900

    fix: 생년월일로 미래를 입력할 경우 disabled & toast

--- src/features/member/components/new/BirthInputForm.tsx
+++ src/features/member/components/new/BirthInputForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useController, useFormContext } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
 
@@ -8,25 +8,42 @@ import BirthIcon from '@/assets/icons/birth-icon.svg';
 import { Button } from '@/components';
 import { MAX_DATE_LENGTH_UNTIL_DAY } from '@/constants';
 import { DateInput } from '@/features/goal/components/new/DateInput';
+import { useToast } from '@/hooks/useToast';
 
 import type { NewMemberFormValues } from '../../types';
 
 import FormLayout from './FormLayout';
 
 export const BirthInputForm = () => {
+  const toast = useToast();
   const router = useRouter();
   const { register, getValues, control } = useFormContext<NewMemberFormValues>();
   const { field } = useController({ name: 'birth', control });
   const { onChange, value } = field;
   const { nickname } = getValues();
-
-  const isInvalidInput = () => (value ? value.length !== MAX_DATE_LENGTH_UNTIL_DAY : true);
+  const [isValidBirth, setIsValidBirth] = useState(false);
 
   useEffect(() => {
     if (!nickname) {
       router.push('/member/new/nickname');
     }
   }, []);
+
+  useEffect(() => {
+    if (value) {
+      if (value.length === MAX_DATE_LENGTH_UNTIL_DAY) {
+        const today = new Date();
+        const inputValue = new Date(value);
+
+        if (today < inputValue) {
+          toast.warning('올바른 생년월일을 입력해주세요.');
+          setIsValidBirth(false);
+          return;
+        }
+      }
+      setIsValidBirth(value.length === MAX_DATE_LENGTH_UNTIL_DAY);
+    }
+  }, [value]);
 
   return (
     <FormLayout
@@ -39,7 +56,7 @@ export const BirthInputForm = () => {
           <div {...register('birth')}>
             <DateInput maxLength={MAX_DATE_LENGTH_UNTIL_DAY} onChange={onChange} />
           </div>
-          <Button type="submit" disabled={isInvalidInput()}>
+          <Button type="submit" disabled={!isValidBirth}>
             완료
           </Button>
         </div>
