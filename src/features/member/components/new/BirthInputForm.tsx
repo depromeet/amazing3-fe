ecbecaa403c1d32ed84315225bbb5d6e0ee1e23c
Doeunnkimm <doeunnkimm@gmail.com>
2024-01-27 13:46:50 +0900

    fix: 정보수정 페이지에도 생년월일 유효성 검사 추가

--- src/features/member/components/new/BirthInputForm.tsx
+++ src/features/member/components/new/BirthInputForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useController, useFormContext } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
 
@@ -8,42 +8,25 @@ import BirthIcon from '@/assets/icons/birth-icon.svg';
 import { Button } from '@/components';
 import { MAX_DATE_LENGTH_UNTIL_DAY } from '@/constants';
 import { DateInput } from '@/features/goal/components/new/DateInput';
-import { useToast } from '@/hooks/useToast';
+import { useValidBirth } from '@/hooks/useValidBirth';
 
 import type { NewMemberFormValues } from '../../types';
 
 import FormLayout from './FormLayout';
 
 export const BirthInputForm = () => {
-  const toast = useToast();
   const router = useRouter();
   const { register, getValues, control } = useFormContext<NewMemberFormValues>();
   const { field } = useController({ name: 'birth', control });
   const { onChange, value } = field;
+  const isValidBirth = useValidBirth(value);
   const { nickname } = getValues();
-  const [isValidBirth, setIsValidBirth] = useState(false);
 
   useEffect(() => {
     if (!nickname) {
       router.push('/member/new/nickname');
     }
   }, []);
-
-  useEffect(() => {
-    if (value) {
-      if (value.length === MAX_DATE_LENGTH_UNTIL_DAY) {
-        const today = new Date();
-        const inputValue = new Date(value);
-
-        if (today < inputValue) {
-          toast.warning('올바른 생년월일을 입력해주세요.');
-          setIsValidBirth(false);
-          return;
-        }
-      }
-      setIsValidBirth(value.length === MAX_DATE_LENGTH_UNTIL_DAY);
-    }
-  }, [value]);
 
   return (
     <FormLayout
---
+++ src/hooks/useValidBirth.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+import { MAX_DATE_LENGTH_UNTIL_DAY } from '@/constants';
+
+import { useToast } from './useToast';
+
+export const useValidBirth = (value: string) => {
+  const toast = useToast();
+  const [isValid, setIsValid] = useState(false);
+
+  useEffect(() => {
+    if (value) {
+      if (value.length === MAX_DATE_LENGTH_UNTIL_DAY) {
+        const today = new Date();
+        const inputValue = new Date(value);
+
+        if (today < inputValue) {
+          toast.warning('올바른 생년월일을 입력해주세요.');
+          setIsValid(false);
+          return;
+        }
+      }
+      setIsValid(value.length === MAX_DATE_LENGTH_UNTIL_DAY);
+    }
+  }, [value]);
+
+  return isValid;
+};
