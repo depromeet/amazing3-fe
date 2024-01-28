4302f30c8a5e298427258d7875d7696beeedb3a4
deepbig <34956359+deepbig@users.noreply.github.com>
2024-01-29 00:14:20 +0900

    bug: 남의 지도에서 빈 목표 카드 선택 시 로직 분리

--- src/features/home/components/MapCardCollections/EmptyMapCard/EmptyMapCard.tsx
+++ src/features/home/components/MapCardCollections/EmptyMapCard/EmptyMapCard.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useOverlay } from '@toss/use-overlay';
+import { useAtomValue } from 'jotai';
 
 import bandiboodiGray from '@/assets/images/bandi-boodi-gray.png';
 import { Typography } from '@/components';
+import { isLoginAtom } from '@/features/auth/atom';
 import { useIsMyMap } from '@/hooks';
 
+import { LoginBottomSheet } from '../../loginBottomSheet';
 import { MapCardLayout, type MapCardLayoutProps } from '../MapCardLayout';
 
 interface EmptyMapCardProps extends MapCardLayoutProps {
@@ -14,16 +18,30 @@ interface EmptyMapCardProps extends MapCardLayoutProps {
 const EMPTY_ALTERNATIVE_TEXTS = ['나의 3년 후는?', '목표 생각중..', '나는 갓생러 ㅋ', '성공이 뭘까?'];
 
 export const EmptyMapCard = ({ alternativeTextIndex, position }: EmptyMapCardProps) => {
+  const router = useRouter();
+  const isLogin = useAtomValue(isLoginAtom);
   const { isMyMap } = useIsMyMap();
+  const { open } = useOverlay();
+
+  const handleMapCardClick = () => {
+    if (isMyMap) {
+      router.push(`/goal/new/goal`);
+    } else if (isLogin) {
+      // 로그인한 사용자가 내 지도가 아닌 카드를 클릭했을 때 어떤 작업을 해야하는지 의논 필요.
+      return;
+    } else {
+      open(({ isOpen, close }) => <LoginBottomSheet open={isOpen} onClose={close} />);
+    }
+  };
 
   return (
-    <Link href={{ pathname: isMyMap ? '/goal/new/goal' : '' }}>
+    <button onClick={handleMapCardClick}>
       <MapCardLayout position={position} cursor="default">
         <Image src={bandiboodiGray} width="100" height="100" alt="empty_goal" />
         <Typography type="title5" className="text-gray-40 text-center font-bold">
           {EMPTY_ALTERNATIVE_TEXTS[alternativeTextIndex]}
         </Typography>
       </MapCardLayout>
-    </Link>
+    </button>
   );
 };
