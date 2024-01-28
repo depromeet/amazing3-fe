f9417ddf4359ac182c06bec9a08812f6acae95cd
deepbig <34956359+deepbig@users.noreply.github.com>
2024-01-28 23:36:48 +0900

    bug: 불필요한 accessToken 체크 로직 삭제

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,3 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
-
-export async function middleware(request: NextRequest) {
-  if (process.env.NODE_ENV !== 'development') {
-    const token = request.cookies.get('accessToken');
-
-    return token ? NextResponse.next() : NextResponse.redirect(new URL('/', request.url));
-  }
-}
-
 export const config = {
   matcher: ['/goal/:path*', '/member/:path*', '/home/:path*'],
 };
