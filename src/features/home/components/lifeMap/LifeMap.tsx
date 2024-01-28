2e0f38d3609b2845dc461e393b25abb5f17bfa98
deepbig <34956359+deepbig@users.noreply.github.com>
2024-01-28 19:55:18 +0900

    bug: 처음 isMyMap 값이 false로 시작되어 open/life-map이 호출되는 문제 해결
    localStorage가 없는 경우를 잡기 위해 useEffect 사용

--- src/features/home/components/lifeMap/LifeMap.tsx
+++ src/features/home/components/lifeMap/LifeMap.tsx
@@ -1,18 +1,15 @@
 'use client';
 
-import { CURRENT_LIFEMAP_USERNAME_KEY } from '@/constants/storageKey';
 import { PrivateLifeMap } from '@/features/home/components/lifeMap/PrivateLifeMap';
 import { PublicLifeMap } from '@/features/home/components/lifeMap/PublicLifeMap';
+import { useIsMyMap } from '@/hooks';
 
 interface LifeMapContainerProps {
   username: string;
 }
 
 export const LifeMap = ({ username }: LifeMapContainerProps) => {
-  localStorage.setItem(CURRENT_LIFEMAP_USERNAME_KEY, username);
+  const { isMyMap } = useIsMyMap(username);
 
-  const currentUsername = localStorage.getItem('username');
-  const isMyMap = username === currentUsername;
-
-  return isMyMap ? <PrivateLifeMap /> : <PublicLifeMap username={username} />;
+  return isMyMap === null ? <></> : isMyMap ? <PrivateLifeMap /> : <PublicLifeMap username={username} />;
 };
