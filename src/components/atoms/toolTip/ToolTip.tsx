b01ced6e5cefa2031a9c75730a57b63aeaaae3f1
김도은 <doeunnkimm@gmail.com>
2024-01-28 13:09:32 +0900

    bug: 세부목표 추가 스텝 부분 이슈 및 빠진 부분 추가 (#140)
    * bug: 메모입력 필드 글자수 카운트 안 되던 이슈 해결
    
    * bug: 메모 글자수 많은 경우 cta 버튼과 메모 디스크립션 겹침 이슈 해결
    
    * bug: 목표 저장완료 시 상단 “<”버튼 제거
    
    * chore: 변경된 디자인 시스템 반영
    
    * chore: 세부목표 추가 완료 시 ToolTip 추가
    
    * chore: 쉐도우 적용

--- src/components/atoms/toolTip/ToolTip.tsx
+++ src/components/atoms/toolTip/ToolTip.tsx
@@ -9,11 +9,11 @@ interface ToolTipProps {
 
 export const ToolTip = ({ title }: ToolTipProps) => {
   return (
-    <div className="w-fit px-3xs py-5xs flex items-center justify-center bg-blue-30 rounded-[12px] relative">
-      <Typography type="title5" className="text-white">
+    <div className="w-fit px-3xs py-5xs flex items-center justify-center bg-blue-10 rounded-[12px] relative">
+      <Typography type="title4" className="text-blue-30">
         {title}
       </Typography>
-      <ReverseTriangleIcon width={14} height={10} fill={colors.blue[30]} className="absolute bottom-[-9px]" />
+      <ReverseTriangleIcon width={14} height={10} fill={colors.blue[10]} className="absolute bottom-[-9px]" />
     </div>
   );
 };
