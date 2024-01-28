8df6411f05c3e2f01e24e0c75a285c75d7d39587
minkyung00 <newminkyung@gmail.com>
2024-01-28 14:01:01 +0900

    feat: 응원한 사람이 없을 때 보여주는 ui 추가

--- src/features/like/LikeClickerList.tsx
+++ src/features/like/LikeClickerList.tsx
@@ -8,9 +8,12 @@ import LikeIcon from '@/assets/icons/like-icon.png';
 import { Typography } from '@/components';
 import { formatOver999 } from '@/utils/number';
 
+import EmptyLikeClicker from './EmptyLikeClicker';
 import LikeClicker from './LikeClicker';
 
 const LikeClickerList = () => {
+  // FIXME: 추후 api로 수정 예정
+  const likeClickerCount = 0;
   const router = useRouter();
 
   const handleClickBack = () => {
@@ -26,21 +29,27 @@ const LikeClickerList = () => {
         </Typography>
       </div>
 
-      <div className="flex items-center justify-center px-2xs py-[14.5px] mx-2xs my-[10px] rounded-md bg-gradient-to-r to-[#FCF3FF] from-[#EAF1FF]">
-        <Typography className="font-insungit text-[13px] text-purple-30">
-          <Image src={LikeIcon} alt="like-button-image" width={24} height={24} className="inline-block mr-[8px]" />
+      {!likeClickerCount ? (
+        <EmptyLikeClicker />
+      ) : (
+        <>
+          <div className="flex items-center justify-center px-2xs py-[14.5px] mx-2xs my-[10px] rounded-md bg-gradient-to-r to-[#FCF3FF] from-[#EAF1FF]">
+            <Typography className="font-insungit text-[13px] text-purple-30">
+              <Image src={LikeIcon} alt="like-button-image" width={24} height={24} className="inline-block mr-[8px]" />
 
-          <span>{`${formatOver999(1000)}명`}</span>
-          <span className="text-[#6884F6]">의 반디가 내 인생 지도를 응원했어요 !</span>
-        </Typography>
-      </div>
+              <span>{`${formatOver999(1000)}명`}</span>
+              <span className="text-[#6884F6]">의 반디가 내 인생 지도를 응원했어요 !</span>
+            </Typography>
+          </div>
 
-      {/* FIXME: api 수정 예정 */}
-      <div className="flex flex-col gap-6xs mx-[24px]">
-        <LikeClicker image="/" nickname="유저 닉네임" likedAt="2023.08.23" />
-        <LikeClicker image="/" nickname="유저 닉네임" likedAt="2023.08.23" />
-        <LikeClicker image="/" nickname="유저 닉네임" likedAt="2023.08.23" />
-      </div>
+          {/* FIXME: api 수정 예정 */}
+          <div className="flex flex-col gap-6xs mx-[24px]">
+            <LikeClicker image="/" nickname="유저 닉네임" likedAt="2023.08.23" />
+            <LikeClicker image="/" nickname="유저 닉네임" likedAt="2023.08.23" />
+            <LikeClicker image="/" nickname="유저 닉네임" likedAt="2023.08.23" />
+          </div>
+        </>
+      )}
     </div>
   );
 };
