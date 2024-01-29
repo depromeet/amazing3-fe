b780eb7f32311c632b8fb8bd102c53d27ccf5fde
Doeunnkimm <doeunnkimm@gmail.com>
2024-01-29 18:16:04 +0900

    feat: goals에 prefetch hydration 적용

--- src/app/home/[username]/page.tsx
+++ src/app/home/[username]/page.tsx
@@ -1,11 +1,18 @@
+import { api } from '@/apis/server';
+import PrefetchHydration from '@/contexts/reactQuery/PrefetchHydration';
 import { LifeMap } from '@/features/home/components';
+import type { GoalResponse } from '@/hooks/reactQuery/goal/useGetGoal';
 
 interface HomeRouteParams {
   params: { username: string };
 }
 
 const HomePage = ({ params: { username } }: HomeRouteParams) => {
-  return <LifeMap username={username} />;
+  return (
+    <PrefetchHydration queryKey={['goals']} queryFn={() => api.get<GoalResponse>('/life-map')}>
+      <LifeMap username={username} />
+    </PrefetchHydration>
+  );
 };
 
 export default HomePage;
