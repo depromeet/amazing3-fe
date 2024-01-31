6cc7dc6a54cfdf4708cf3c8f7154662ef536a9e3
김도은 <doeunnkimm@gmail.com>
2024-01-31 19:47:58 +0900

    fix: 스플래시 화면에서 뒷 배경 길이 조절 (#168)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ const SplashPage = () => {
       <div className="absolute top-0 right-0">
         <Image src={PurpleBlurImage} sizes="100vw" alt="blur_image" />
       </div>
-      <div className="w-full h-[60vh] bg-gradient1 flex flex-col items-center">
+      <div className="w-full h-[65vh] bg-gradient1 flex flex-col items-center">
         <div className="h-[38vh] flex flex-col gap-[1vh] z-10 justify-center">
           <Typography type="heading2" className="text-center bg-clip-text bg-gradient4 text-transparent">
             내가 직접 그리는 <br /> 나의 인생지도
