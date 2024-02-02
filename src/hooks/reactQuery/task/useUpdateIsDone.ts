4e55aec75c350c601a176ee917d21b1f7d8f0984
김도은 <doeunnkimm@gmail.com>
2024-02-02 15:46:25 +0900

    refactor: 세부목표 isDone 요청에 debounce 추가 (#161)
    * feat: 세부목표 isDone 요청에 debounce 추가
    
    * chore: 낙관적 업데이트 코드 부분 제거
    
    * refactor: 피드백 반영

--- src/hooks/reactQuery/task/useUpdateIsDone.ts
+++ src/hooks/reactQuery/task/useUpdateIsDone.ts
@@ -2,9 +2,6 @@ import { useMutation } from '@tanstack/react-query';
 
 import { api } from '@/apis';
 
-import type { GoalResponse } from '../goal/useGetGoal';
-import { useOptimisticUpdate } from '../useOptimisticUpdate';
-
 type IsDoneRequest = {
   goalId: number;
   taskId: number;
@@ -12,23 +9,7 @@ type IsDoneRequest = {
 };
 
 export const useUpdateIsDone = () => {
-  const { queryClient, optimisticUpdater } = useOptimisticUpdate();
-
   return useMutation({
     mutationFn: ({ taskId, isDone }: IsDoneRequest) => api.patch(`/task/${taskId}/isDone`, { isDone }),
-    onMutate: async ({ goalId, taskId, isDone }) => {
-      const targetQueryKey = ['goal', goalId];
-
-      const updater = (old: GoalResponse): GoalResponse => {
-        const updatedTask = old.tasks.map((task) => (task.taskId === taskId ? { ...task, isTaskDone: isDone } : task));
-        return { ...old, tasks: updatedTask };
-      };
-      const context = await optimisticUpdater({ queryKey: targetQueryKey, updater });
-      return context;
-    },
-    onError: (_, variable, context) => {
-      const targetQueryKey = ['goal', variable.goalId];
-      queryClient.setQueryData(targetQueryKey, context?.previous);
-    },
   });
 };
