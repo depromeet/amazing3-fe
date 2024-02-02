805d64ec32614929ac9b827aa0e92404f313e322
minkyung00 <newminkyung@gmail.com>
2024-02-02 22:42:34 +0900

    feat: 응원하기에 throttle 적용

--- src/features/home/components/lifeMap/PublicLifeMap.tsx
+++ src/features/home/components/lifeMap/PublicLifeMap.tsx
@@ -5,7 +5,9 @@ import Link from 'next/link';
 import { useOverlay } from '@toss/use-overlay';
 
 import { Button } from '@/components/atoms';
+import { CHEER_INTERVAL } from '@/constants';
 import { CheeringButton } from '@/features/cheering/CheeringButton';
+import { useThrottle } from '@/hooks';
 import { useGetMemberData } from '@/hooks/reactQuery/auth';
 import { useCreateCheering } from '@/hooks/reactQuery/cheering';
 import { useGetPublicGoals } from '@/hooks/reactQuery/goal/useGetPublicGoals';
@@ -18,11 +20,13 @@ import { LifeMapContent } from './LifeMapContent';
 export const PublicLifeMap = ({ username }: { username: string }) => {
   const { data: memberData } = useGetMemberData();
   const { data: publicGoals } = useGetPublicGoals({ username });
-  const { mutate, isSuccess } = useCreateCheering(username);
+
   const { open } = useOverlay();
   const [isCheeringSuccess, setIsCheeringSuccess] = useState(false);
 
   // TODO: Lottie atom을 수정해서 로티 이미지를 플레이하는 방식으로 변경
+  const { mutate: cheer, isSuccess } = useCreateCheering(username);
+
   useEffect(() => {
     let timeoutId: NodeJS.Timeout | undefined;
     if (isSuccess) {
@@ -41,15 +45,15 @@ export const PublicLifeMap = ({ username }: { username: string }) => {
 
   const myHomePath = memberData?.username ? `/home/${memberData.username}` : '/';
 
+  const throttleCheer = useThrottle(() => cheer({ lifeMapId: publicGoals?.lifeMapId }), CHEER_INTERVAL);
+
   const handleClickCheeringButton = () => {
     if (!memberData) {
       open(({ isOpen, close }) => <LoginBottomSheet open={isOpen} onClose={close} />);
       return;
     }
 
-    // TODO: 디바운스 처리 / 1분
-    // 1분 전일 때 toast로 이미 응원했다고 알림.
-    mutate({ lifeMapId: publicGoals?.lifeMapId });
+    throttleCheer();
   };
 
   return (
