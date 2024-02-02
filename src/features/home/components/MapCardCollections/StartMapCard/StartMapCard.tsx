83bb65152c1b723ddd647112eecb0706602a466f
Doeunnkimm <doeunnkimm@gmail.com>
2024-02-03 01:23:52 +0900

    chore: 줄바꿈 되지 않도록

--- src/features/home/components/MapCardCollections/StartMapCard/StartMapCard.tsx
+++ src/features/home/components/MapCardCollections/StartMapCard/StartMapCard.tsx
@@ -9,7 +9,7 @@ import { MapCardLayout } from '../MapCardLayout';
 export const StartMapCard = ({ position }: StartMapCardProps) => {
   return (
     <MapCardLayout position={position} cursor="default">
-      <Typography type="title5" className="text-blue-55 text-center pt-[10px] font-bold">
+      <Typography type="title5" className="text-blue-55 text-center pt-[10px] font-bold !whitespace-nowrap">
         START !
       </Typography>
       <div className="absolute w-[235px] h-[235px] top-[10px] left-1/2 transform -translate-x-1/2">
