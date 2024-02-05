04270e50b4160a1b698425973f919f43802119ff
deepbig <34956359+deepbig@users.noreply.github.com>
2024-02-05 20:17:07 +0900

    bug: username에 BANDIBOODI가 포함된 경우 validation 실패 처리

--- src/features/my/contexts/UpdateMemberDataFormProvider.tsx
+++ src/features/my/contexts/UpdateMemberDataFormProvider.tsx
@@ -5,6 +5,7 @@ import { DevTool } from '@hookform/devtools';
 
 import { useUpdateMemberData } from '@/hooks/reactQuery/auth';
 import { useIsMounted } from '@/hooks/useIsMounted';
+import { useToast } from '@/hooks/useToast';
 
 import type { UpdateMemberDataFormValues } from '../types';
 
@@ -13,6 +14,7 @@ const UpdateMemberDataFormProvider = ({ children }: PropsWithChildren) => {
   const isMounted = useIsMounted();
   const { mutate, isSuccess } = useUpdateMemberData();
   const methods = useForm<UpdateMemberDataFormValues>();
+  const toast = useToast();
 
   useEffect(() => {
     if (isSuccess) {
@@ -23,6 +25,11 @@ const UpdateMemberDataFormProvider = ({ children }: PropsWithChildren) => {
   const submit = (formData: UpdateMemberDataFormValues) => {
     const { nickname, username, birth } = formData;
     if (!nickname || !username || !birth) return;
+
+    if (username.toUpperCase().startsWith('BANDIBOODI-')) {
+      toast.warning('BANDIBOODI-로 시작하는 닉네임은 사용할 수 없습니다.');
+      return;
+    }
 
     // Convert birth from "YYYY.MM.DD" to "YYYY-MM-DD"
     // TODO: 서버에서 YYYY.MM.DD 형식을 지원하면 이 부분 삭제
