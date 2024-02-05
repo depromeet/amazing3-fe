5b710613acc50dc70fc03a488ef9902f53679435
Hongsuk Ryu <34956359+deepbig@users.noreply.github.com>
2024-02-06 00:45:39 +0900

    bug: username에 BANDIBOODI가 포함된 경우 validation 실패 처리 (#178)
    * bug: username에 BANDIBOODI가 포함된 경우 validation 실패 처리
    
    * username에 변경이 없는 경우는 허용

--- src/features/my/contexts/UpdateMemberDataFormProvider.tsx
+++ src/features/my/contexts/UpdateMemberDataFormProvider.tsx
@@ -3,16 +3,19 @@ import { FormProvider, useForm } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
 import { DevTool } from '@hookform/devtools';
 
-import { useUpdateMemberData } from '@/hooks/reactQuery/auth';
+import { useGetMemberData, useUpdateMemberData } from '@/hooks/reactQuery/auth';
 import { useIsMounted } from '@/hooks/useIsMounted';
+import { useToast } from '@/hooks/useToast';
 
 import type { UpdateMemberDataFormValues } from '../types';
 
 const UpdateMemberDataFormProvider = ({ children }: PropsWithChildren) => {
   const router = useRouter();
   const isMounted = useIsMounted();
+  const { data: memberData } = useGetMemberData();
   const { mutate, isSuccess } = useUpdateMemberData();
   const methods = useForm<UpdateMemberDataFormValues>();
+  const toast = useToast();
 
   useEffect(() => {
     if (isSuccess) {
@@ -23,6 +26,11 @@ const UpdateMemberDataFormProvider = ({ children }: PropsWithChildren) => {
   const submit = (formData: UpdateMemberDataFormValues) => {
     const { nickname, username, birth } = formData;
     if (!nickname || !username || !birth) return;
+
+    if (memberData?.username !== username && username.toUpperCase().startsWith('BANDIBOODI-')) {
+      toast.warning('BANDIBOODI-로 시작하는 닉네임은 사용할 수 없습니다.');
+      return;
+    }
 
     // Convert birth from "YYYY.MM.DD" to "YYYY-MM-DD"
     // TODO: 서버에서 YYYY.MM.DD 형식을 지원하면 이 부분 삭제
