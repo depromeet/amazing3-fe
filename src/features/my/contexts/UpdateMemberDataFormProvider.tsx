5ef5d6df4a89f6eaf17865b9d64b5136af13e692
deepbig <34956359+deepbig@users.noreply.github.com>
2024-02-13 21:56:40 +0900

    bug: username 변경 시 제한 조건 추가

--- src/features/my/contexts/UpdateMemberDataFormProvider.tsx
+++ src/features/my/contexts/UpdateMemberDataFormProvider.tsx
@@ -27,8 +27,15 @@ const UpdateMemberDataFormProvider = ({ children }: PropsWithChildren) => {
     const { nickname, username, birth } = formData;
     if (!nickname || !username || !birth) return;
 
+    // TODO: react-hook-form을 사용하는 input field는 별도로 component로 분리해서 에러 상태에 대한 처리를 할 수 있도록 수정
     if (memberData?.username !== username && username.toUpperCase().startsWith('BANDIBOODI-')) {
       toast.warning('BANDIBOODI-로 시작하는 닉네임은 사용할 수 없습니다.');
+      return;
+    }
+
+    // Username may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen.
+    if (!/^[a-zA-Z0-9]+(-?[a-zA-Z0-9])*[a-zA-Z0-9]$/.test(username)) {
+      toast.warning('닉네임은 영문, 숫자, 하이픈(-)만 사용할 수 있고, 하이픈(-)은 처음과 끝에 사용할 수 없습니다.');
       return;
     }
 
