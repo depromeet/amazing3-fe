26f756ee939822cbdfddff37f464f19f26222aed
Doeunnkimm <doeunnkimm@gmail.com>
2024-02-14 15:18:40 +0900

    chore: 세부목표 페이지에 EmojiSet 적용

--- src/features/goal/components/detail/ContentBody.tsx
+++ src/features/goal/components/detail/ContentBody.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
+import { EmojiSet } from '@/components';
 import { Span, Typography } from '@/components/atoms';
+import type { EmojiSetProps } from '@/components/molecules/emojiSet';
 
 interface ContentBodyProps {
   title: string;
@@ -8,6 +10,16 @@ interface ContentBodyProps {
   tag: string;
   more: string;
 }
+
+// mock
+const emojis: EmojiSetProps = {
+  like: 10,
+  cheer: 10,
+  angry: 10,
+  sad: 100,
+  awesome: 100,
+  surprised: 100,
+};
 
 export const ContentBody = ({ title, date, tag, more }: ContentBodyProps) => {
   return (
@@ -17,6 +29,7 @@ export const ContentBody = ({ title, date, tag, more }: ContentBodyProps) => {
         <Typography type="title3" className="text-gray-40">
           <Span type="blue55">{date}</Span>까지 이룰 거에요 | <Span type="blue55">{tag}</Span>
         </Typography>
+        <EmojiSet {...emojis} />
         <Typography type="body3" className="text-gray-40">
           {more}
         </Typography>
