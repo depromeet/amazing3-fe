215d512ee81833804889619bd758606c98d72ed3
deepbig <34956359+deepbig@users.noreply.github.com>
2024-02-17 21:02:39 +0900

    레이아웃 버그 수정 및 한글 문법 수정

--- src/app/goal/detail/layout.tsx
+++ src/app/goal/detail/layout.tsx
@@ -1,7 +1,7 @@
 import type { PropsWithChildren } from 'react';
 
 const layout = ({ children }: PropsWithChildren) => {
-  return <main className="relative h-screen">{children}</main>;
+  return <main className="relative h-[100dvh]">{children}</main>;
 };
 
 export default layout;
