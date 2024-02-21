e096d2dcd1d811d9aafbf30a9f85fe5fdd702aa1
hjy0951 <hjy0951@gmail.com>
2024-02-21 18:19:44 +0900

    feat: FeedCard 컴포넌트 초기 구현

--- src/features/feed/FeedBody.tsx
+++ src/features/feed/FeedBody.tsx
@@ -1,3 +1,22 @@
+'use client';
+
+import { useGetGoalFeeds } from '@/hooks/reactQuery/goal';
+
+import FeedCard from './FeedCard';
+
 export const FeedBody = () => {
-  return <div>body</div>;
+  const { data: goalFeedsData } = useGetGoalFeeds();
+
+  console.log(goalFeedsData?.goals);
+
+  // TODO: 피드가 0개일 때, 처리 필요
+
+  return (
+    <div>
+      <div className="mx-xs my-xs flex flex-col gap-md">
+        {goalFeedsData &&
+          goalFeedsData.goals.map((goalData) => <FeedCard key={goalData.goal.id} feedData={goalData} />)}
+      </div>
+    </div>
+  );
 };
---
+++ src/features/feed/FeedCard.tsx
@@ -0,0 +1,71 @@
+import Image from 'next/image';
+
+import ForwardIcon from '@/assets/icons/forward-icon.svg';
+import VerticalBarIcon from '@/assets/icons/vertical-bar.svg';
+import { Avatar, Span, Typography } from '@/components';
+import { blueDataURL } from '@/constants';
+import type { GoalFeedProps } from '@/hooks/reactQuery/goal/useGetGoalFeeds';
+
+interface FeedCardProps {
+  feedData: GoalFeedProps;
+}
+
+const FeedCard = ({ feedData: { user, goal, count } }: FeedCardProps) => {
+  return (
+    <div className="flex flex-col gap-4xs">
+      {/* Card Header */}
+      <div className="flex justify-between items-center">
+        <div className="flex gap-5xs">
+          <Avatar size={40} profileImage={user.image} />
+          <div className="flex flex-col">
+            <Typography type="title4" className="text-gray-50">
+              {user.nickname}
+            </Typography>
+            <Typography type="caption1" className="text-gray-40">
+              작성 시간
+            </Typography>
+          </div>
+        </div>
+        <button className="px-4xs py-6xs flex gap-6xs bg-blue-10 rounded-md">
+          <Typography type="title5" className="text-blue-30">
+            {count.goal} 개 목표조각 보러가기
+          </Typography>
+          <ForwardIcon width={16} fill="#69B1FF" />
+        </button>
+      </div>
+      {/* Card Body */}
+      <div>
+        <div></div>
+        <div className="p-3xs flex flex-col gap-5xs bg-gray-10 rounded-lg">
+          <div className="flex gap-5xs items-center">
+            <Image
+              src={goal.sticker}
+              width={48}
+              height={48}
+              alt="goal sticker"
+              priority
+              placeholder="blur"
+              blurDataURL={blueDataURL.mapCard}
+            />
+            <Typography type="heading4">{goal.title}</Typography>
+          </div>
+          <div className="flex items-center gap-5xs">
+            <Typography type="title5" className="text-gray-40">
+              <Span type="gray50">{goal.deadline}</Span>까지 이룰거에요
+            </Typography>
+            <VerticalBarIcon width="2" height="16" />
+            <Typography type="title5" className="text-gray-50">
+              {goal.tag}
+            </Typography>
+            <VerticalBarIcon width="2" height="16" />
+            <Typography type="title5" className="text-gray-40">
+              세부목표 <Span type="gray50">{count.task}개</Span>
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedCard;
