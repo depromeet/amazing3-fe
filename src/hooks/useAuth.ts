ae50297b87db6fbb375f8ed7f6842bfe016e4d20
Doeunnkimm <doeunnkimm@gmail.com>
2024-02-22 16:04:51 +0900

    refactor: useAuth에 login, logout 추가 후 리팩터링

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,12 +1,24 @@
+import { useRouter } from 'next/navigation';
 import { useAtom } from 'jotai';
+import Cookies from 'js-cookie';
 
 import { isLoggedInAtom } from '@/atoms';
 
 export const useAuth = () => {
+  const router = useRouter();
   const [isLoggedIn, setIsLoggedIn] = useAtom(isLoggedInAtom);
-  const username = localStorage.getItem('username');
 
-  const handleLogout = () => setIsLoggedIn(false);
+  const handleLogin = (token: string) => {
+    Cookies.set('accessToken', token, { secure: process.env.NODE_ENV !== 'development', expires: 7 });
+    setIsLoggedIn(true);
+  };
 
-  return { isLoggedIn, username, logout: handleLogout };
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    Cookies.remove('accessToken');
+    localStorage.clear();
+    router.replace('/');
+  };
+
+  return { isLoggedIn, login: handleLogin, logout: handleLogout };
 };
