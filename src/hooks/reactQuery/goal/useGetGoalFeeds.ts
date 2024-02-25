748531fb0054c9db0d438baea8985af0b08853b5
hjy0951 <hjy0951@gmail.com>
2024-02-25 14:37:17 +0900

    chore: /goal/explore API 추가 사항 반영

--- src/hooks/reactQuery/goal/useGetGoalFeeds.ts
+++ src/hooks/reactQuery/goal/useGetGoalFeeds.ts
@@ -6,6 +6,7 @@ export type GoalFeedProps = {
   user: {
     id: number;
     nickname: string;
+    username: string;
     image: string;
   };
   goal: {
@@ -15,6 +16,7 @@ export type GoalFeedProps = {
     deadline: string;
     sticker: string;
     tag: string;
+    createdAt: string;
   };
   count: {
     reaction: number;
