dd5ceef74284c675d5c6d818e55bc2501fecb202
Doeunnkimm <doeunnkimm@gmail.com>
2024-02-25 21:39:43 +0900

    chore: CountEmoji 컴포넌트 추가

---
+++ src/components/molecules/countEmoji/CountEmoji.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { CountEmoji } from './CountEmoji';
+
+const meta: Meta<typeof CountEmoji> = {
+  title: 'components/molecules/countEmoji',
+  component: CountEmoji,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof CountEmoji>;
+
+export const Basic: Story = {
+  args: {
+    src: 'https://github.com/depromeet/amazing3-fe/assets/112946860/5c103242-786d-4700-bf32-b87e4c4a8970',
+    name: 'like',
+    count: 10,
+  },
+};
+
+export const Selected: Story = {
+  args: {
+    ...Basic.args,
+    reacted: true,
+  },
+};
+
+export const OnlyView: Story = {
+  args: {
+    ...Basic.args,
+    onlyView: true,
+  },
+};
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,4 +1,5 @@
 export { ContentWrapper } from './contentWrapper';
+export { CountEmoji } from './countEmoji';
 export { InfiniteScroller } from './infiniteScroll';
 export { LimitedLengthInput } from './limitedLengthInput';
 export { LoginIconSet } from './loginIconSet';
