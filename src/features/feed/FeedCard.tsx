9fddac111e0cdb186a4b87b2ef26c6e07d4552d9
hjy0951 <hjy0951@gmail.com>
2024-02-25 15:26:48 +0900

    feat: 피드의 목표 조각 보러가기, 해당 목표 보러가기 Link 추가

--- src/features/feed/FeedCard.tsx
+++ src/features/feed/FeedCard.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import ForwardIcon from '@/assets/icons/forward-icon.svg';
 import VerticalBarIcon from '@/assets/icons/vertical-bar.svg';
@@ -23,47 +24,51 @@ const FeedCard = ({ feedData: { user, goal, count } }: FeedCardProps) => {
               {user.nickname}
             </Typography>
             <Typography type="caption1" className="text-gray-40">
-              작성 시간
+              {goal.createdAt}
             </Typography>
           </div>
         </div>
-        <button className="px-4xs py-6xs flex gap-6xs bg-blue-10 rounded-md">
-          <Typography type="title5" className="text-blue-30">
-            {count.goal} 개 목표조각 보러가기
-          </Typography>
-          <ForwardIcon width={16} fill="#69B1FF" />
-        </button>
+        <Link href={{ pathname: `/home/${user.username}` }}>
+          <button className="px-4xs py-6xs flex gap-6xs bg-blue-10 rounded-md">
+            <Typography type="title5" className="text-blue-30">
+              {count.goal} 개 목표조각 보러가기
+            </Typography>
+            <ForwardIcon width={16} fill="#69B1FF" />
+          </button>
+        </Link>
       </div>
       {/* Card Body */}
       <div className="relative">
         <div className="absolute top-0 left-3xs right-0 bottom-0 w-1 bg-gray-10 rounded-full" />
-        <div className=" ml-lg p-3xs flex flex-col gap-5xs bg-gray-10 rounded-lg">
-          <div className="flex gap-5xs items-center">
-            <Image
-              src={goal.sticker}
-              width={48}
-              height={48}
-              alt="goal sticker"
-              priority
-              placeholder="blur"
-              blurDataURL={blueDataURL.mapCard}
-            />
-            <Typography type="heading4">{goal.title}</Typography>
+        <Link href={{ pathname: `/goal/detail/${goal.id}` }}>
+          <div className="ml-lg p-3xs flex flex-col gap-5xs bg-gray-10 rounded-lg">
+            <div className="flex gap-5xs items-center">
+              <Image
+                src={goal.sticker}
+                width={48}
+                height={48}
+                alt="goal sticker"
+                priority
+                placeholder="blur"
+                blurDataURL={blueDataURL.mapCard}
+              />
+              <Typography type="heading4">{goal.title}</Typography>
+            </div>
+            <div className="flex items-center gap-5xs">
+              <Typography type="title5" className="text-gray-40">
+                <Span type="gray50">{formatDotYYYYMM(goal.deadline)}</Span>까지 이룰거에요
+              </Typography>
+              <VerticalBarIcon width="2" height="16" />
+              <Typography type="title5" className="text-gray-50">
+                {goal.tag}
+              </Typography>
+              <VerticalBarIcon width="2" height="16" />
+              <Typography type="title5" className="text-gray-40">
+                세부목표 <Span type="gray50">{count.task}개</Span>
+              </Typography>
+            </div>
           </div>
-          <div className="flex items-center gap-5xs">
-            <Typography type="title5" className="text-gray-40">
-              <Span type="gray50">{formatDotYYYYMM(goal.deadline)}</Span>까지 이룰거에요
-            </Typography>
-            <VerticalBarIcon width="2" height="16" />
-            <Typography type="title5" className="text-gray-50">
-              {goal.tag}
-            </Typography>
-            <VerticalBarIcon width="2" height="16" />
-            <Typography type="title5" className="text-gray-40">
-              세부목표 <Span type="gray50">{count.task}개</Span>
-            </Typography>
-          </div>
-        </div>
+        </Link>
       </div>
     </div>
   );
