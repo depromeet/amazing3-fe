1fe0a7d05bd5e9861866e48549ed071a79eb7d01
Doeunnkimm <doeunnkimm@gmail.com>
2024-02-28 16:28:18 +0900

    chore: Tabs에서 Trigger 컴포넌트만 불러와서 사용

--- src/features/goal/components/detail/emoji/BottomSheet/Tab.tsx
+++ src/features/goal/components/detail/emoji/BottomSheet/Tab.tsx
@@ -1,13 +1,14 @@
 import type { PropsWithChildren } from 'react';
-import * as Tabs from '@radix-ui/react-tabs';
+import type { TabsTriggerProps } from '@radix-ui/react-tabs';
+import { Trigger } from '@radix-ui/react-tabs';
 
-export const Tab = ({ children, ...props }: PropsWithChildren<Tabs.TabsTriggerProps>) => {
+export const Tab = ({ children, ...props }: PropsWithChildren<TabsTriggerProps>) => {
   return (
-    <Tabs.Trigger
+    <Trigger
       {...props}
       className="flex-shrink-0 pb-4xs data-[state=active]:text-purple-40 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0]"
     >
       {children}
-    </Tabs.Trigger>
+    </Trigger>
   );
 };
