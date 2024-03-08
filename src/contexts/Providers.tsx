4ce089e6695fbdfa19050939ae7771f3851488c1
Doeunnkimm <doeunnkimm@gmail.com>
2024-03-08 22:14:18 +0900

    chore: import React 문 추가

--- src/contexts/Providers.tsx
+++ src/contexts/Providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import type { PropsWithChildren } from 'react';
+import React from 'react';
 import { OverlayProvider } from '@toss/use-overlay';
 import { domAnimation, LazyMotion } from 'framer-motion';
 
