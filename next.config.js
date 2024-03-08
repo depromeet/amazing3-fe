861a84abd11bec6f752e2a2ce9d35da01d28a683
minkyung00 <newminkyung@gmail.com>
2024-03-08 17:36:24 +0900

    chore: next 설정에 sentry 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withSentryConfig } = require('@sentry/nextjs');
 
 const nextConfig = {
   reactStrictMode: true,
@@ -61,4 +63,9 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+const sentryWebpackPluginOptions = {
+  silent: true,
+  authToken: process.env.NEXT_PUBLIC_SENTRY_AUTH_TOKEN,
+};
+
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
