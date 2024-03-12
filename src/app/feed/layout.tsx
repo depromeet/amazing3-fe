f95332e5a31ed6fdace8030a75227aabddddad02
hjy0951 <hjy0951@gmail.com>
2024-03-13 00:01:00 +0900

    design: 피드 페이지 헤더 수정

--- src/app/feed/layout.tsx
+++ src/app/feed/layout.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from 'react';
 const FeedPageLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="w-full h-[100dvh] bg-white">
-      <div className="flex flex-col gap-3xs pt-5xs">{children}</div>
+      <div className="flex flex-col pt-5xs">{children}</div>
     </div>
   );
 };
--- src/features/feed/FeedHeader.tsx
+++ src/features/feed/FeedHeader.tsx
@@ -1,23 +1,11 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-
-import BackIcon from '@/assets/icons/goal/back-icon.svg';
 import { Typography } from '@/components/atoms';
 
 export const FeedHeader = () => {
-  const router = useRouter();
-
-  const handleClickBack = () => {
-    router.back();
-  };
-
   return (
-    <div className="flex items-center px-[24px]">
-      <button type="button" onClick={handleClickBack}>
-        <BackIcon />
-      </button>
-      <Typography type="title1" className="absolute left-[50%] -translate-x-1/2 text-gray-70">
+    <div className="px-[24px] py-[10px]">
+      <Typography type="title1" className="text-gray-70">
         목표피드
       </Typography>
     </div>
