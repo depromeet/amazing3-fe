854c294bb01d4afb805bf13244bfc6cd0ee5dc22
Doeunnkimm <doeunnkimm@gmail.com>
2024-03-13 23:48:56 +0900

    chore: props명 수정

--- src/components/molecules/countEmoji/CountEmoji.tsx
+++ src/components/molecules/countEmoji/CountEmoji.tsx
@@ -2,24 +2,30 @@ import { Emoji } from '@/components';
 import type { EmojiProps } from '@/components/atoms/emoji';
 
 interface CountEmojiProps extends EmojiProps {
-  count: number;
-  reacted?: boolean;
+  reactCount: number;
+  isMyReaction?: boolean;
   onlyView?: boolean;
   onClick?: VoidFunction;
 }
 
-export const CountEmoji = ({ count, reacted = false, onlyView = false, onClick, ...props }: CountEmojiProps) => {
+export const CountEmoji = ({
+  reactCount,
+  isMyReaction = false,
+  onlyView = false,
+  onClick,
+  ...props
+}: CountEmojiProps) => {
   return (
     <button
-      className={`p-[3px] rounded-[8px] ${!reacted ? 'bg-white' : 'bg-blue-10'} ${
+      className={`p-[3px] rounded-[8px] ${!isMyReaction ? 'bg-white' : 'bg-blue-10'} ${
         !onlyView && 'transition-shadow duration-500 hover:shadow-thumb'
       }`}
       disabled={onlyView}
       onClick={onClick}
     >
       <Emoji {...props} />
-      <p className={`text-[12px] ${!reacted ? 'text-gray-70' : 'text-blue-50'} leading-[155%] tracking-[-0.6px]`}>
-        {count}
+      <p className={`text-[12px] ${!isMyReaction ? 'text-gray-70' : 'text-blue-50'} leading-[155%] tracking-[-0.6px]`}>
+        {reactCount}
       </p>
     </button>
   );
