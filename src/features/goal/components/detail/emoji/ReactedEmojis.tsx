f580b60dc05f77c41a1554e12dee6ff6487c45ad
Doeunnkimm <doeunnkimm@gmail.com>
2024-03-13 23:55:44 +0900

    feat: 이모지 추가/삭제 기능 추가 및 적용

--- src/features/goal/components/detail/emoji/ReactedEmojis.tsx
+++ src/features/goal/components/detail/emoji/ReactedEmojis.tsx
@@ -1,28 +1,39 @@
 import { m } from 'framer-motion';
+import { useAtomValue } from 'jotai';
 
 import { CountEmoji } from '@/components';
+import { goalIdAtom } from '@/features/goal/atom';
+import { useCreateEmoji, useDeleteReactedEmoji } from '@/hooks/reactQuery/emoji';
 
 interface ReactedEmojis {
   onCloseEmojis: VoidFunction;
   reactedEmojis: {
+    id: number;
     url: string;
     name: string;
-    count: number;
-    reacted: boolean;
+    reactCount: number;
+    isMyReaction: boolean;
   }[];
 }
 
 export const ReactedEmojis = ({ onCloseEmojis, reactedEmojis }: ReactedEmojis) => {
-  const handleClick = () => {
-    // TODO: 리액션 추가 api 작업
+  const goalId = useAtomValue(goalIdAtom);
+
+  const { mutate: deleteReactedEmoji } = useDeleteReactedEmoji();
+  const { mutate: createReactEmoji } = useCreateEmoji();
+
+  const handleClick = (emojiId: number, isMyReaction: boolean) => () => {
+    if (isMyReaction) deleteReactedEmoji({ emojiId, goalId });
+    else createReactEmoji({ emojiId, goalId });
+
     onCloseEmojis();
   };
 
   return (
     <div className="flex gap-5xs">
       {reactedEmojis.map((emoji) => (
-        <m.div key={emoji.url} whileTap={{ scale: 1.1 }} transition={{ duration: 0.2 }}>
-          <CountEmoji {...emoji} size={36} onClick={handleClick} />
+        <m.div key={emoji.url} whileTap={{ scale: 1.1 }} transition={{ duration: 0.2 }} className="w-[40px]">
+          <CountEmoji {...emoji} size={36} onClick={handleClick(emoji.id, emoji.isMyReaction)} />
         </m.div>
       ))}
     </div>
