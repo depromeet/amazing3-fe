aaf6ecebbf401814032c871dcd93d4aeaf1c7bfc
minkyung00 <newminkyung@gmail.com>
2024-03-13 22:50:37 +0900

    fix: key 중복 수정을 위한 유틸 함수 추가

--- src/features/cheering/CheererList.tsx
+++ src/features/cheering/CheererList.tsx
@@ -2,6 +2,7 @@
 
 import { InfiniteScroller } from '@/components/molecules';
 import { useGetCheerer } from '@/hooks/reactQuery/cheering';
+import { generateId } from '@/utils/generateId';
 
 import { Cheerer } from './Cheerer';
 import { CheererCount } from './CheererCount';
@@ -16,10 +17,10 @@ export const CheererList = () => {
     <>
       <CheererCount count={cheererList?.pages[0].total} />
 
-      <InfiniteScroller isLastPage={!hasNextPage} onIntersected={() => fetchNextPage()}>
+      <InfiniteScroller isLastPage={!hasNextPage} onIntersect={() => fetchNextPage()}>
         <div className="flex flex-col gap-6xs mx-[24px] h-[calc(100dvh - 80px)]">
           {cheererList?.pages.map((page) =>
-            page.contents.map(({ ...props }) => <Cheerer key={`${props.userId}-${props.cheeringAt}`} {...props} />),
+            page.contents.map(({ ...props }) => <Cheerer key={generateId()} {...props} />),
           )}
         </div>
       </InfiniteScroller>
