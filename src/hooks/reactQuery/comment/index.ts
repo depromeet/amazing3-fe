361f58024de6c3cdc977d3c2d5e6fdac5809d920
Doeunnkimm <doeunnkimm@gmail.com>
2024-03-15 00:11:58 +0900

    feat: 댓글 조회 기능 추가

---
+++ src/hooks/reactQuery/comment/index.ts
@@ -0,0 +1,1 @@
+export { useGetComment } from './useGetComment';
---
+++ src/hooks/reactQuery/comment/useGetComment.ts
@@ -0,0 +1,33 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { api } from '@/apis';
+
+type CommentRequestParams = {
+  goalId: number;
+};
+
+export type CommentResponse = {
+  comments: CommentProps[];
+  commentCount: number;
+};
+
+type CommentProps = {
+  id: number;
+  content: string;
+  writtenAt: string;
+  commenter: CommenterProps;
+  isMyComment: boolean;
+};
+
+type CommenterProps = {
+  username: string;
+  nickname: string;
+  image: string;
+};
+
+export const useGetComment = ({ goalId }: CommentRequestParams) => {
+  return useQuery<CommentResponse>({
+    queryKey: ['comment', goalId],
+    queryFn: () => api.get<CommentResponse>(`/goal/${goalId}/comment`),
+  });
+};
