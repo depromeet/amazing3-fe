5fef14bc9f584ada03349ce39e59b5501e3fcbc4
minkyung <newminkyung@gmail.com>
2024-03-14 19:06:33 +0900

    feat: 바텀 네비게이션 컴포넌트 구현 (#202)
    * chore: 아이콘 에셋 추가
    
    * fix: 유저 아이콘 fill 속성 값 변경
    
    * feat: 바텀 네비게이션 내 MainMenu 컴포넌트 구현
    
    * feat: 바텀 네비게이션 내 BottomMenu 컴포넌트 구현
    
    * feat: 바텀 네비게이션 구현
    
    * feat: 내 지도인 경우, 바텀 네비게이션 추가
    
    * feat: transition 속성 추가

--- src/features/home/components/lifeMap/PrivateLifeMap.tsx
+++ src/features/home/components/lifeMap/PrivateLifeMap.tsx
@@ -1,10 +1,8 @@
 import { useRef } from 'react';
-import dynamic from 'next/dynamic';
 
+import { BottomNavigation } from '@/components/molecules/bottomNavigation';
 import { useGetMemberData } from '@/hooks/reactQuery/auth';
 import { useGetGoals } from '@/hooks/reactQuery/goal/useGetGoals';
-
-const PrivateLifeMapBottomArea = dynamic(() => import('@/features/home/components/lifeMap/PrivateLifeMapBottomArea'));
 
 import { LifeMapContent } from './LifeMapContent';
 
@@ -17,7 +15,7 @@ export const PrivateLifeMap = () => {
   return (
     <>
       <LifeMapContent goalsData={privateGoals} memberData={memberData} downloadSectionRef={downloadSectionRef} />
-      <PrivateLifeMapBottomArea downloadSectionRef={downloadSectionRef} />
+      <BottomNavigation />
     </>
   );
 };
