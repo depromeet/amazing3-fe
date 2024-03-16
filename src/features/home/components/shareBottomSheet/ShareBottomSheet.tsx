48d55a0c1dff5af07810a299ccb239ed5e14319b
minkyung <newminkyung@gmail.com>
2024-03-17 00:08:30 +0900

    feat: 내 지도에서 프로필 이미지 -> 공유 버튼으로 변경  (#210)
    * chore: 공유 아이콘 변경
    
    * feat: 공유 버튼 클릭 시, 공유 바텀 시트 보이도록 수정
    
    * feat: 내 지도에서 프로필 -> 공유 버튼으로 변경
    
    * chore: 사용하지 않는 ref 삭제
    
    * chore: 하단 탭 추가로 인해 사용하지 않는 컴포넌트 삭제
    
    * fix: 사용하지 않는 props 삭제

--- src/features/home/components/shareBottomSheet/ShareBottomSheet.tsx
+++ src/features/home/components/shareBottomSheet/ShareBottomSheet.tsx
@@ -1,4 +1,3 @@
-import { forwardRef } from 'react';
 import Image from 'next/image';
 
 import BandiBoodiShare from '@/assets/images/bandiboodi-share.png';
@@ -12,29 +11,23 @@ interface ShareGoalBottomSheetProps {
   onClickImageDownload: () => void;
 }
 
-export const ShareBottomSheet = forwardRef<HTMLElement, ShareGoalBottomSheetProps>(
-  ({ open, onClose, onClickImageDownload }: ShareGoalBottomSheetProps, ref) => {
-    return (
-      <BottomSheet
-        open={open}
-        onDismiss={onClose}
-        fixedMaxHeight={480}
-        FooterComponent={
-          <ShareBottomSheetFooter ref={ref} onClose={onClose} onClickImageDownload={onClickImageDownload} />
-        }
-      >
-        <div className="flex flex-col items-center justify-center gap-3xs translate-y-[20px]">
-          <Typography type="title1" className="text-gray-70 text-center">
-            {`인생지도를\n공유하시겠어요?`}
-          </Typography>
-          <Typography type="body3" className="text-gray-50">
-            친구랑 함께 쓰면 즐거움이 두배!
-          </Typography>
-          <Image src={BandiBoodiShare} width={250} alt="bandiboodi-share" />
-        </div>
-      </BottomSheet>
-    );
-  },
-);
-
-ShareBottomSheet.displayName = 'ShareBottomSheet';
+export const ShareBottomSheet = ({ open, onClose, onClickImageDownload }: ShareGoalBottomSheetProps) => {
+  return (
+    <BottomSheet
+      open={open}
+      onDismiss={onClose}
+      fixedMaxHeight={480}
+      FooterComponent={<ShareBottomSheetFooter onClose={onClose} onClickImageDownload={onClickImageDownload} />}
+    >
+      <div className="flex flex-col items-center justify-center gap-3xs translate-y-[20px]">
+        <Typography type="title1" className="text-gray-70 text-center">
+          {`인생지도를\n공유하시겠어요?`}
+        </Typography>
+        <Typography type="body3" className="text-gray-50">
+          친구랑 함께 쓰면 즐거움이 두배!
+        </Typography>
+        <Image src={BandiBoodiShare} width={250} alt="bandiboodi-share" />
+      </div>
+    </BottomSheet>
+  );
+};
--- src/features/home/components/shareBottomSheet/ShareBottomSheetFooter.tsx
+++ src/features/home/components/shareBottomSheet/ShareBottomSheetFooter.tsx
@@ -1,5 +1,3 @@
-import { forwardRef } from 'react';
-
 import { Button } from '@/components';
 import { shareLink } from '@/utils/share';
 
@@ -8,26 +6,22 @@ interface ShareBottomSheetFooterProps {
   onClickImageDownload: VoidFunction;
 }
 
-const ShareBottomSheetFooter = forwardRef<HTMLElement, ShareBottomSheetFooterProps>(
-  ({ onClose, onClickImageDownload }: ShareBottomSheetFooterProps) => {
-    const handleCopyClipboard = () => {
-      shareLink({ url: location.href });
-      onClose();
-    };
+const ShareBottomSheetFooter = ({ onClose, onClickImageDownload }: ShareBottomSheetFooterProps) => {
+  const handleCopyClipboard = () => {
+    shareLink({ url: location.href });
+    onClose();
+  };
 
-    return (
-      <div className="flex gap-5xs mb-5xs">
-        <Button variant="blue" onClick={handleCopyClipboard}>
-          URL 복사
-        </Button>
-        <Button variant="heavy" onClick={onClickImageDownload}>
-          이미지 저장
-        </Button>
-      </div>
-    );
-  },
-);
-
-ShareBottomSheetFooter.displayName = 'ShareBottomSheetFooter';
+  return (
+    <div className="flex gap-5xs mb-5xs">
+      <Button variant="blue" onClick={handleCopyClipboard}>
+        URL 복사
+      </Button>
+      <Button variant="heavy" onClick={onClickImageDownload}>
+        이미지 저장
+      </Button>
+    </div>
+  );
+};
 
 export default ShareBottomSheetFooter;
