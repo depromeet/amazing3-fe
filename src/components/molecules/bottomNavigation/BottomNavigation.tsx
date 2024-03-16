f507b069c3f0b3e62095f5d84ee22b4bb4623663
minkyung00 <newminkyung@gmail.com>
2024-03-17 00:29:16 +0900

    feat: 바텀 네비게이션 메뉴 active 조건 수정 및 스타일 수정

--- src/components/molecules/bottomNavigation/BottomNavigation.tsx
+++ src/components/molecules/bottomNavigation/BottomNavigation.tsx
@@ -17,11 +17,10 @@ export const BottomNavigation = () => {
   const isHome = pathname?.startsWith('/home');
 
   return (
-    <div className="absolute bottom-0 flex justify-center gap-[48px] w-full pt-5xs pb-xs px-xs shadow-thumb">
-      {/* FIXME: href, active pathname 수정 필요 */}
-      <SubMenu href="/" active={pathname === '/'} Icon={FeedIcon} menuName="목표 피드" />
+    <div className="sticky bottom-0 flex justify-center gap-[48px] w-full bg-white pt-5xs pb-xs px-xs shadow-thumb">
+      <SubMenu href="/feed" active={pathname === '/feed'} Icon={FeedIcon} menuName="목표 피드" />
       <MainMenu href={isHome ? '/goal/new/goal' : pathname} Icon={isHome ? PlusIcon : HomeIcon} />
-      <SubMenu href="/my" active={pathname === '/'} Icon={UserIcon} menuName="마이" />
+      <SubMenu href="/my" active={pathname === '/my'} Icon={UserIcon} menuName="마이" />
     </div>
   );
 };
