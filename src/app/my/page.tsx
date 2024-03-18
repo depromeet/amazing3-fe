6ede0f61c01016a6075af8cc86c1784c0feec3db
minkyung <newminkyung@gmail.com>
2024-03-19 00:01:06 +0900

    feat: 피드 페이지 / 마이 페이지에 바텀 네비게이션 추가 (#217)
    * feat: 바텀 네비게이션 메뉴 active 조건 수정 및 스타일 수정
    
    * feat: 마이페이지, 피드페이지에 바텀네비게이션 추가
    
    * chore: 레이아웃 스타일 수정
    
    * fix: 마이페이지에서 홈 버튼 클릭 시, 홈 화면으로 이동하도록 수정
    
    * fix: 레이아웃 시프트 현상 해결
    
    * refactor: 필요한 모듈만 import 하도록 (#214)
    
    * chore: 이모지 추가 로직 중 낙관적 업데이트 제거 (#215)
    
    * fix: 남의 지도일 때, 공유 버튼 -> 프로필 이미지로 변경 (#220)
    
    * chore: totalReactUserCount 값 추가 및 적용 (#222)
    
    * chore: 내 지도 스타일 변경 (#212)
    
    * chore: 목표 개수, 응원 개수 텍스트 내용 수정
    
    * chore: 폰트 크기 변경
    
    * feat: 지도 응답 값 title 추가, tagContent 삭제
    
    * feat: 지도 카드 내용을 태그 날짜 -> 목표명으로 변경
    
    * fix: 바텀 네비게이션 z-index 수정
    
    * fix: 지도 페이지네이션 컴포넌트 잘려보이는 이슈 수정
    
    ---------
    
    Co-authored-by: Doeun Kim <doeunnkimm@gmail.com>

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -1,7 +1,13 @@
+import { BottomNavigation } from '@/components/molecules/bottomNavigation';
 import { MyPageLayout } from '@/features/my/components';
 
 const MyPage = () => {
-  return <MyPageLayout />;
+  return (
+    <>
+      <MyPageLayout />
+      <BottomNavigation />
+    </>
+  );
 };
 
 export default MyPage;
