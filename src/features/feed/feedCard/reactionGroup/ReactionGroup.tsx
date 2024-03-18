b5ce2460b4a4186ab37e5dab2b7aa47c37af6ff3
hjy0951 <hjy0951@gmail.com>
2024-03-18 21:11:55 +0900

    chore: 주석 삭제

--- src/features/feed/feedCard/reactionGroup/ReactionGroup.tsx
+++ src/features/feed/feedCard/reactionGroup/ReactionGroup.tsx
@@ -6,16 +6,6 @@ import type { EmojisProps } from '@/hooks/reactQuery/goal/useGetGoalFeeds';
 import { Emojis } from './Emojis';
 import { ReactedEmojiButton } from './ReactedEmojiButton';
 
-// TODO: API 연결 후 삭제 예정
-// const reactedEmojis = [
-//   { url: emojisData[0].url, name: emojisData[0].name, count: 3, isMyReaction: true },
-//   { url: emojisData[0].url, name: emojisData[0].name, count: 27, isMyReaction: false },
-//   { url: emojisData[0].url, name: emojisData[0].name, count: 123, isMyReaction: true },
-//   { url: emojisData[0].url, name: emojisData[0].name, count: 12313, isMyReaction: true },
-//   { url: emojisData[0].url, name: emojisData[0].name, count: 11, isMyReaction: false },
-//   { url: emojisData[0].url, name: emojisData[0].name, count: 999, isMyReaction: false },
-// ];
-
 interface ReactionGroupProps {
   targetGoalId: number;
   reactedEmojis: Array<EmojisProps>;
