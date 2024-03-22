c28f9997d200fb788b5cf7bfe0027b042968683d
Heojoooon <45158550+hjy0951@users.noreply.github.com>
2024-03-22 16:20:13 +0900

    feat: 피드 페이지 API 연결 (#224)
    * feat: /goal/explore API 변경 사항 적용
    
    * refactor: /goal/explore 응답 값 타입 수정
    
    * feat: 연결된 API ReactionGroup에 적용
    
    * refactor: Window -> Emojis로 이름 변경시 누락된 변수들 수정
    
    * chore: 주석 삭제
    
    * feat: GET /emoji API 연결
    
    * feat: 이모지 API 연결
    
    * fix: 모바일 환경에서 이모지 선택창 글씨가 두줄로 넘어가는 현상 수정
    
    * chore: 불필요한 상태 제거

--- src/features/feed/feedCard/reactionGroup/ReactionGroup.tsx
+++ src/features/feed/feedCard/reactionGroup/ReactionGroup.tsx
@@ -1,36 +1,46 @@
 import { useState } from 'react';
 
 import { ReactionAddButton } from '@/features/emoji';
+import type { EmojisProps } from '@/hooks/reactQuery/goal/useGetGoalFeeds';
 
-import { Emojis, emojisData } from './Emojis';
+import { Emojis } from './Emojis';
 import { ReactedEmojiButton } from './ReactedEmojiButton';
 
-// TODO: API 연결 후 삭제 예정
-const reactedEmojis = [
-  { url: emojisData[0].url, name: emojisData[0].name, count: 3, isMyReaction: true },
-  { url: emojisData[0].url, name: emojisData[0].name, count: 27, isMyReaction: false },
-  { url: emojisData[0].url, name: emojisData[0].name, count: 123, isMyReaction: true },
-  { url: emojisData[0].url, name: emojisData[0].name, count: 12313, isMyReaction: true },
-  { url: emojisData[0].url, name: emojisData[0].name, count: 11, isMyReaction: false },
-  { url: emojisData[0].url, name: emojisData[0].name, count: 999, isMyReaction: false },
-];
+interface ReactionGroupProps {
+  targetGoalId: number;
+  reactedEmojis: Array<EmojisProps>;
+}
 
-export const ReactionGroup = () => {
-  const [isOpenWindow, setOpenWindow] = useState(false);
+export const ReactionGroup = ({ targetGoalId, reactedEmojis }: ReactionGroupProps) => {
+  const [isOpenEmojis, setOpenEmojis] = useState(false);
 
-  const handleToggleWindow = () => {
-    setOpenWindow((prev) => !prev);
+  const handleToggleEmojis = () => {
+    setOpenEmojis((prev) => !prev);
+  };
+  const handleCloseEmojis = () => {
+    () => setOpenEmojis(false);
   };
 
   return (
     <>
       <div className="w-full  flex flex-wrap gap-5xs items-center">
-        {reactedEmojis.map(({ url, name, count, isMyReaction }, idx) => (
-          <ReactedEmojiButton key={idx} url={url} name={name} count={count} isMyReaction={isMyReaction} />
+        {reactedEmojis.map(({ id, url, name, reactCount, isMyReaction }) => (
+          <ReactedEmojiButton
+            key={id}
+            goalId={targetGoalId}
+            emojiId={id}
+            url={url}
+            name={name}
+            count={reactCount}
+            isMyReaction={isMyReaction}
+            onCloseEmojis={handleCloseEmojis}
+          />
         ))}
-        <ReactionAddButton isOpen={isOpenWindow} onClick={handleToggleWindow} />
+        <ReactionAddButton isOpen={isOpenEmojis} onClick={handleToggleEmojis} />
+        <div className="relative w-full">
+          {isOpenEmojis && <Emojis goalId={targetGoalId} onToggle={handleToggleEmojis} />}
+        </div>
       </div>
-      <div className="relative w-full">{isOpenWindow && <Emojis onToggle={handleToggleWindow} />}</div>
     </>
   );
 };
