87f1f6610088fa8f41a4753a5913ac9f32196710
Doeunnkimm <doeunnkimm@gmail.com>
2024-03-26 20:35:51 +0900

    refactor: 변경된 디자인 적용 및 기능 리팩터링

--- src/features/goal/utils/date.ts
+++ src/features/goal/utils/date.ts
@@ -1,4 +1,8 @@
-export const formatDate = (splitedDate: string[], separator: string) => {
+import { DATE_SEPARATOR } from '@/constants';
+import type { DateProps } from '@/hooks/useDateInput';
+import { typeToMaxLength } from '@/hooks/useDateInput';
+
+export const formatDate = (splitedDate: string[], separator = DATE_SEPARATOR) => {
   return splitedDate.join(separator);
 };
 
@@ -10,4 +14,17 @@ export const isValidDate = (year: string, month: string, day: string) => {
   const date = new Date(yearNum, monthNum, dayNum);
 
   return date.getFullYear() === yearNum && date.getMonth() === monthNum && date.getDate() === dayNum;
+};
+
+/**
+ *
+ * @param string DATE_SEPARATOR를 기준으로 split했을 때, 모든 아이템이 number여야만, 길이를 만족해야만 true를 반환합니다.
+ * @param format ex. 'YYYY.MM.DD'
+ */
+export const isValidDateFormat = (formattedDate = '', format: string) => {
+  const splittedFormat = format.split(DATE_SEPARATOR) as DateProps[];
+
+  return formattedDate
+    .split(DATE_SEPARATOR)
+    .every((date, index) => !isNaN(+date) && date.length === typeToMaxLength[splittedFormat[index]]);
 };
