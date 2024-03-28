974fd2075d8daa96f9b8f42656861b4547afc75a
hjy0951 <hjy0951@gmail.com>
2024-03-28 15:56:20 +0900

    feat: 피드 이모지 추가/삭제시 낙관적 업데이트 적용

--- src/hooks/reactQuery/emoji/useCreateEmojiForFeed.ts
+++ src/hooks/reactQuery/emoji/useCreateEmojiForFeed.ts
@@ -1,20 +1,86 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import type { InfiniteData } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 import { api } from '@/apis';
+
+import type { GoalFeedProps, GoalFeedResponse } from '../goal/useGetGoalFeeds';
+import { useOptimisticUpdate } from '../useOptimisticUpdate';
+
+import type { Emoji, EmojisResponse } from './useGetAllEmoji';
 
 type EmojiRequestParams = {
   goalId: number;
   emojiId: number;
 };
 
+interface GoalFeedInfinityResponse extends InfiniteData<GoalFeedResponse, unknown> {}
+
+const NOT_FOUNDED_INDEX = -1;
+
 export const useCreateEmojiForFeed = () => {
-  const queryClient = useQueryClient();
+  const { queryClient, optimisticUpdater } = useOptimisticUpdate();
+  const targetQueryKey = ['goalFeeds'];
 
   return useMutation({
     mutationFn: ({ goalId, emojiId }: EmojiRequestParams) => api.post(`/goal/${goalId}/emoji/${emojiId}`),
+    onMutate: async ({ goalId, emojiId }) => {
+      const updater = (old: GoalFeedInfinityResponse): GoalFeedInfinityResponse => {
+        const newData = JSON.parse(JSON.stringify(old)) as GoalFeedInfinityResponse;
+        const reactEmojiData = queryClient
+          .getQueryData<EmojisResponse>(['all-emoji'])
+          ?.find((emoji) => emoji.id === emojiId) as Emoji;
+
+        let currentPage,
+          pageIndex = NOT_FOUNDED_INDEX,
+          goalIndex = NOT_FOUNDED_INDEX;
+
+        while (pageIndex < newData.pages.length && goalIndex < 0) {
+          pageIndex++;
+          currentPage = newData.pages[pageIndex];
+          goalIndex = currentPage.goals.findIndex(({ goal }) => goal.id === goalId);
+        }
+
+        if (!currentPage) return old;
+
+        const targetGoal = currentPage.goals[goalIndex];
+        const targetEmojis = targetGoal.emojis;
+        const existedEmojiIndex = targetEmojis.findIndex(({ id }) => id === emojiId);
+
+        let newEmoji = null;
+
+        if (existedEmojiIndex === NOT_FOUNDED_INDEX) {
+          newEmoji = { ...reactEmojiData, reactCount: 1, isMyReaction: true };
+        } else {
+          const targetEmoji = targetEmojis[existedEmojiIndex];
+          newEmoji = { ...targetEmoji, reactCount: targetEmoji.reactCount + 1 };
+          targetEmojis.splice(existedEmojiIndex, 1, newEmoji);
+        }
+
+        const newEmojis = existedEmojiIndex >= 0 ? targetEmojis : [...targetEmojis, newEmoji];
+        const newGoal = {
+          ...targetGoal,
+          emojis: newEmojis,
+          count: {
+            ...targetGoal.count,
+            reaction: targetGoal.count.reaction + 1,
+          },
+        } as GoalFeedProps;
+        currentPage.goals.splice(goalIndex, 1, newGoal);
+
+        return newData;
+      };
+
+      const context = await optimisticUpdater({
+        queryKey: targetQueryKey,
+        updater,
+      });
+      return context;
+    },
     onSuccess: (_, { goalId }) => {
-      queryClient.invalidateQueries({ queryKey: ['goalFeeds'] });
       queryClient.invalidateQueries({ queryKey: ['emoji', goalId] });
+    },
+    onError: (_, __, context) => {
+      queryClient.setQueryData(targetQueryKey, context?.previous);
     },
   });
 };
--- src/hooks/reactQuery/emoji/useDeleteReactedEmojiForFeed.ts
+++ src/hooks/reactQuery/emoji/useDeleteReactedEmojiForFeed.ts
@@ -1,20 +1,70 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import type { InfiniteData } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 import { api } from '@/apis';
+
+import type { GoalFeedResponse } from '../goal/useGetGoalFeeds';
+import { useOptimisticUpdate } from '../useOptimisticUpdate';
 
 type EmojiRequestParams = {
   goalId: number;
   emojiId: number;
 };
 
+interface GoalFeedInfinityResponse extends InfiniteData<GoalFeedResponse, unknown> {}
+
+const NOT_FOUNDED_INDEX = -1;
+
 export const useDeleteReactedEmojiForFeed = () => {
-  const queryClient = useQueryClient();
+  // const queryClient = useQueryClient();
+  const { queryClient, optimisticUpdater } = useOptimisticUpdate();
+  const targetQueryKey = ['goalFeeds'];
 
   return useMutation({
     mutationFn: ({ goalId, emojiId }: EmojiRequestParams) => api.delete(`/goal/${goalId}/emoji/${emojiId}`),
+    onMutate: async ({ goalId, emojiId }) => {
+      const updater = (old: GoalFeedInfinityResponse): GoalFeedInfinityResponse => {
+        const newData = JSON.parse(JSON.stringify(old)) as GoalFeedInfinityResponse;
+
+        let currentPage,
+          pageIndex = NOT_FOUNDED_INDEX,
+          goalIndex = NOT_FOUNDED_INDEX;
+
+        while (pageIndex < newData.pages.length && goalIndex < 0) {
+          pageIndex++;
+          currentPage = newData.pages[pageIndex];
+          goalIndex = currentPage.goals.findIndex(({ goal }) => goal.id === goalId);
+        }
+
+        if (!currentPage) return old;
+
+        const targetGoal = currentPage.goals[goalIndex];
+        const targetEmojis = targetGoal.emojis;
+        const existedEmojiIndex = targetEmojis.findIndex(({ id }) => id === emojiId);
+
+        if (existedEmojiIndex === NOT_FOUNDED_INDEX) {
+          return old;
+        } else {
+          const targetEmoji = targetEmojis[existedEmojiIndex];
+          targetEmoji.reactCount--;
+          if (targetEmoji.reactCount === 0) targetEmojis.splice(existedEmojiIndex, 1);
+        }
+
+        return newData;
+      };
+
+      const context = await optimisticUpdater({
+        queryKey: targetQueryKey,
+        updater,
+      });
+      return context;
+    },
     onSuccess: (_, { goalId }) => {
-      queryClient.invalidateQueries({ queryKey: ['goalFeeds'] });
+      // queryClient.invalidateQueries({ queryKey: ['goalFeeds'] });
       queryClient.invalidateQueries({ queryKey: ['emoji', goalId] });
+    },
+    onError: (_, __, context) => {
+      queryClient.setQueryData(targetQueryKey, context?.previous);
     },
   });
 };
