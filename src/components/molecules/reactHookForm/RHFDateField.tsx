c360db8493ebcfc73fc2a66825aeb6ecf4c92836
Hongsuk Ryu <34956359+deepbig@users.noreply.github.com>
2024-04-02 22:23:27 +0900

    feat: 목표 수정 기능 개발 (#192)
    * 레이아웃 버그 수정 및 한글 문법 수정
    
    * Textarea atom 버그 수정
    
    * React Hook Form을 사용하는 TextField molecule 생성
    
    * GoalUpdateForm 추가
    
    * 불필요한 코드 삭제 및 RHFTextField.tsx에 Controller 반영
    
    * RHFTextField를 textarea, input 태그 둘 다 핸들링 할 수 있게 수정
    
    * RHFTextField 함수를 더 읽기 좋게 수정
    
    * RHFDateField 생성
    
    * RHFImagePicker와 RHFTagPicker 생성
    
    * RHFTextField 로직 분리
    
    * GoalUpdateForm 로직 완성
    
    * 테스트하기 위해 추가된 promise 제거
    
    * fix build error

---
+++ src/components/molecules/reactHookForm/RHFDateField.tsx
@@ -0,0 +1,108 @@
+import { type InputHTMLAttributes, useEffect, useState } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+
+import { Input, Typography } from '@/components/atoms';
+
+export interface DateFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  helperText?: string;
+  isDayIncluded?: boolean;
+}
+
+export const RHFDateField = ({ label, helperText, isDayIncluded = false }: DateFieldProps) => {
+  return (
+    <div className="space-y-5xs">
+      {label && <Label label={label} />}
+      <div className="grid grid-cols-3 gap-2">
+        <YearField />
+        <MonthField />
+        {isDayIncluded && <DayField />}
+        {helperText && <HelperText text={helperText} />}
+      </div>
+    </div>
+  );
+};
+
+RHFDateField.displayName = 'RHFDateField';
+
+const Label = ({ label }: { label: string }) => (
+  <div className="flex justify-between items-center">
+    <Typography type="title3" className="text-gray-50">
+      {label}
+    </Typography>
+  </div>
+);
+
+const YearField = () => {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name="year"
+      control={control}
+      rules={{ required: 'Year is required' }}
+      render={({ field, fieldState: { error } }) => (
+        <Input {...field} type="number" id="year" placeholder="YYYY" aria-invalid={error ? 'true' : 'false'} />
+      )}
+    />
+  );
+};
+
+const MonthField = () => {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name="month"
+      control={control}
+      rules={{ required: 'Month is required', min: 1, max: 12 }}
+      render={({ field, fieldState: { error } }) => (
+        <Input {...field} type="number" id="month" placeholder="MM" aria-invalid={error ? 'true' : 'false'} />
+      )}
+    />
+  );
+};
+
+const DayField = () => {
+  const { control, watch, setValue, getValues } = useFormContext();
+  const year = watch('year');
+  const month = watch('month');
+
+  // State to hold the maximum number of days for the current month/year
+  const [maxDays, setMaxDays] = useState(31);
+
+  useEffect(() => {
+    if (!year || !month) return;
+
+    // Calculate the maximum number of days in the given month/year
+    const daysInMonth = new Date(year, month, 0).getDate();
+    setMaxDays(daysInMonth);
+  }, [year, month]);
+
+  return (
+    <Controller
+      name="day"
+      control={control}
+      rules={{ required: 'Day is required', min: 1, max: maxDays }}
+      render={({ field, fieldState: { error } }) => (
+        <Input
+          {...field}
+          type="number"
+          id="day"
+          placeholder="DD"
+          min="1"
+          max={maxDays}
+          aria-invalid={error ? 'true' : 'false'}
+        />
+      )}
+    />
+  );
+};
+
+const HelperText = ({ text }: { text: string }) => (
+  <div className="flex flex-col justify-center px-5xs">
+    <Typography type="body3" className="text-gray-40">
+      {text}
+    </Typography>
+  </div>
+);
