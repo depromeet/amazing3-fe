7611622ffad62c11d6fe29bd2f75bfad5f4eaa29
Heojoooon <45158550+hjy0951@users.noreply.github.com>
2024-04-02 23:00:17 +0900

    fix: 피드 페이지에서 댓글 API 요청시 누락된 목표 ID 추가 (#239)

--- src/features/feed/feedCard/ViewCommentButton.tsx
+++ src/features/feed/feedCard/ViewCommentButton.tsx
@@ -1,22 +1,20 @@
 import type { ButtonHTMLAttributes } from 'react';
 import { useOverlay } from '@toss/use-overlay';
-import { useAtomValue } from 'jotai';
 
 import CommentIcon from '@/assets/icons/comment-icon.svg';
 import { Typography } from '@/components';
 import { CommentsBottomSheet } from '@/features/comment';
-import { goalIdAtom } from '@/features/goal/atoms';
 
 interface CommentButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  targetGoalId: number;
   numberOfComments: number;
 }
 
-export const CommentButton = ({ numberOfComments, ...props }: CommentButtonProps) => {
+export const CommentButton = ({ numberOfComments, targetGoalId, ...props }: CommentButtonProps) => {
   const { open } = useOverlay();
-  const goalId = useAtomValue(goalIdAtom);
 
   const handleOpenComments = () => {
-    open(({ isOpen, close }) => <CommentsBottomSheet open={isOpen} onClose={close} goalId={goalId} />);
+    open(({ isOpen, close }) => <CommentsBottomSheet open={isOpen} onClose={close} goalId={targetGoalId} />);
   };
 
   return (
