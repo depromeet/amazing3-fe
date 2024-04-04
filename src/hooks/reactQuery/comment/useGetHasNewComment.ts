7a7cd2d25627ee7a00eb1eb336c3044edfda020e
Doeun Kim <doeunnkimm@gmail.com>
2024-04-04 21:47:46 +0900

    fix: 댓글 리스트 조회 시 빨콩 바로 없어지도록 (#242)

--- src/hooks/reactQuery/comment/useGetHasNewComment.ts
+++ src/hooks/reactQuery/comment/useGetHasNewComment.ts
@@ -5,15 +5,16 @@ import { api } from '@/apis';
 type CommentRequestParams = {
   isMyGoal: boolean;
   goalId: number;
+  enabled: boolean;
 };
 
 type CommentResponse = boolean;
 
-export const useGetHasNewComment = ({ goalId, isMyGoal }: CommentRequestParams) => {
+export const useGetHasNewComment = ({ goalId, isMyGoal, enabled }: CommentRequestParams) => {
   return useQuery<CommentResponse>({
     queryKey: ['comment', 'new', goalId],
     queryFn: () => api.get<CommentResponse>(`/goal/${goalId}/comment/new`),
-    enabled: goalId !== -1 || isMyGoal,
+    enabled: (goalId !== -1 || isMyGoal) && enabled,
     staleTime: 1000,
   });
 };
