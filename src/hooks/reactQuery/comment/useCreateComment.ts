955b4269dca578577099ce33dc81314893260711
minkyung <newminkyung@gmail.com>
2024-04-08 22:40:44 +0900

    bug: 타임라인 댓글 작성 / 삭제 시, 댓글 개수가 반영되지 않는 이슈 수정 (#243)
    * feat: 타임라인 댓글 작성 시, 댓글 개수가 반영이 안되는 이슈 수정
    
    * feat: 타임라인 댓글 삭제 시, 댓글 개수가 반영되지 않는 이슈 수정

--- src/hooks/reactQuery/comment/useCreateComment.ts
+++ src/hooks/reactQuery/comment/useCreateComment.ts
@@ -1,7 +1,12 @@
+import { usePathname } from 'next/navigation';
+import type { InfiniteData } from '@tanstack/react-query';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { api } from '@/apis';
 import { useToast } from '@/hooks';
+
+import type { TimelineResponse } from '../goal/useGetTimeline';
+import { useOptimisticUpdate } from '../useOptimisticUpdate';
 
 type CommentRequest = {
   goalId: number;
@@ -9,14 +14,45 @@ type CommentRequest = {
 };
 
 export const useCreateComment = () => {
+  const pathname = usePathname();
+  const [, , username] = pathname.split('/');
   const toast = useToast();
   const queryClient = useQueryClient();
+  const { queryClient: optimisticQueryClient, optimisticUpdater } = useOptimisticUpdate();
+
+  const targetQueryKey = ['timeline', username];
 
   return useMutation({
     mutationFn: ({ goalId, content }: CommentRequest) => api.post(`/goal/${goalId}/comment`, { content }),
-    onSuccess: (_, variable) => queryClient.invalidateQueries({ queryKey: ['comment', variable.goalId] }),
-    onError: () => {
+    onMutate: async ({ goalId }) => {
+      const updater = (old: InfiniteData<TimelineResponse>) => {
+        const newPages = old.pages.map((page) => ({
+          ...page,
+          contents: page.contents.map((content) =>
+            content.goal.goalId === goalId
+              ? {
+                  ...content,
+                  counts: {
+                    ...content.counts,
+                    comment: content.counts.comment + 1,
+                  },
+                }
+              : content,
+          ),
+        }));
+
+        return { ...old, pages: newPages };
+      };
+
+      return await optimisticUpdater({ queryKey: targetQueryKey, updater });
+    },
+    onSuccess: (_, variable) => {
+      queryClient.invalidateQueries({ queryKey: ['comment', variable.goalId] });
+    },
+    onError: (_, __, context) => {
       toast.warning('잠시후 다시 시도해주세요.');
+
+      optimisticQueryClient.setQueryData(targetQueryKey, context?.previous);
     },
   });
 };
--- src/hooks/reactQuery/comment/useDeleteComment.ts
+++ src/hooks/reactQuery/comment/useDeleteComment.ts
@@ -1,8 +1,11 @@
+import { usePathname } from 'next/navigation';
+import type { InfiniteData } from '@tanstack/react-query';
 import { useMutation } from '@tanstack/react-query';
 
 import { api } from '@/apis';
 import { useToast } from '@/hooks/useToast';
 
+import type { TimelineResponse } from '../goal/useGetTimeline';
 import { useOptimisticUpdate } from '../useOptimisticUpdate';
 
 import type { CommentResponse } from './useGetComment';
@@ -13,6 +16,8 @@ type CommentDeleteRequest = {
 };
 
 export const useDeleteComment = () => {
+  const pathname = usePathname();
+  const [, , username] = pathname.split('/');
   const { queryClient, optimisticUpdater } = useOptimisticUpdate();
   const toast = useToast();
 
@@ -28,8 +33,27 @@ export const useDeleteComment = () => {
       const context = await optimisticUpdater({ queryKey: targetQueryKey, updater });
       return context;
     },
-    onSuccess: () => {
+    onSuccess: (_, { goalId }) => {
       toast.success('댓글을 삭제했어요.');
+
+      queryClient.setQueryData(['timeline', username], (old: InfiniteData<TimelineResponse>) => {
+        const newPages = old?.pages.map((page) => ({
+          ...page,
+          contents: page.contents.map((content) =>
+            content.goal.goalId === goalId
+              ? {
+                  ...content,
+                  counts: {
+                    ...content.counts,
+                    comment: content.counts.comment - 1,
+                  },
+                }
+              : content,
+          ),
+        }));
+
+        return { ...old, pages: newPages };
+      });
     },
     onError: (_, variable, context) => {
       toast.warning('댓글 삭제에 실패했어요.');
