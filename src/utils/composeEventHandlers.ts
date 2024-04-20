5310e322dbfddc01e509d30af5dcc61731bba36d
Doeun Kim <doeunnkimm@gmail.com>
2024-04-20 19:47:45 +0900

    feat: composeEventHandlers 추가 (#258)
    * feat: composeEventHandlers 추가
    
    * fix: composeEventHandlers 적용하여 주입받은 handler가 작동되도록

---
+++ src/utils/composeEventHandlers.ts
@@ -0,0 +1,13 @@
+export const composeEventHandlers = <E>(
+  originalEventHandler?: (event: E) => void,
+  ourEventHandler?: (event: E) => void,
+  { checkForDefaultPrevented = true } = {},
+) => {
+  return function handleEvent(event: E) {
+    originalEventHandler?.(event);
+
+    if (checkForDefaultPrevented === false || !(event as unknown as Event).defaultPrevented) {
+      return ourEventHandler?.(event);
+    }
+  };
+};
