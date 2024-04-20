538777c2d6f72caec06cf1bef427c672dbbbcce1
minkyung00 <newminkyung@gmail.com>
2024-04-20 16:23:11 +0900

    refactor: 피드 이모지 업데이트 훅으로 분리

--- src/hooks/reactQuery/emoji/useCreateEmojiForFeed.ts
+++ src/hooks/reactQuery/emoji/useCreateEmojiForFeed.ts
@@ -1,14 +1,11 @@
-import type { InfiniteData } from '@tanstack/react-query';
 import { useMutation } from '@tanstack/react-query';
 
 import { api } from '@/apis';
+import { useUpdateFeedEmojiCount } from '@/features/home/hooks/useUpdateFeedEmojiCount';
 import { useUpdateTimelineEmojiCount } from '@/features/home/hooks/useUpdateTimelineEmojiCount';
 import { useToast } from '@/hooks';
 
-import type { EmojisProps, GoalFeedResponse } from '../goal/useGetGoalFeeds';
 import { useOptimisticUpdate } from '../useOptimisticUpdate';
-
-import type { Emoji, EmojisResponse } from './useGetAllEmoji';
 
 type EmojiRequestParams = {
   goalId: number;
@@ -19,49 +16,16 @@ export const useCreateEmojiForFeed = () => {
   const toast = useToast();
 
   const { queryClient, optimisticUpdater } = useOptimisticUpdate();
-  const targetQueryKey = ['goalFeeds'];
 
+  const { queryKey: feedQueryKey, updateFeedEmojiCount } = useUpdateFeedEmojiCount({ isAddingEmoji: true });
   const { queryKey: timelineQueryKey, updateTimelineEmojiCount } = useUpdateTimelineEmojiCount({ isAddingEmoji: true });
 
   return useMutation({
     mutationFn: ({ goalId, emojiId }: EmojiRequestParams) => api.post(`/goal/${goalId}/emoji/${emojiId}`),
     onMutate: async ({ goalId, emojiId }) => {
-      const updater = (old: InfiniteData<GoalFeedResponse>) => {
-        if (!old) return null;
-
-        const reactEmojiData = queryClient
-          .getQueryData<EmojisResponse>(['all-emoji'])
-          ?.find((emoji) => emoji.id === emojiId) as Emoji;
-
-        return {
-          ...old,
-          pages: old.pages.map((page) => ({
-            ...page,
-            goals: page.goals.map((goal) =>
-              goal.goal.id === goalId
-                ? {
-                    ...goal,
-                    emojis: goal.emojis.find(({ id }) => id === emojiId)
-                      ? goal.emojis.map((emoji) =>
-                          emoji.id === emojiId
-                            ? { ...emoji, reactCount: emoji.reactCount + 1, isMyReaction: true }
-                            : emoji,
-                        )
-                      : [
-                          ...goal.emojis,
-                          { ...reactEmojiData, id: emojiId, reactCount: 1, isMyReaction: true } as EmojisProps,
-                        ],
-                    count: { ...goal.count, reaction: goal.count.reaction + 1 },
-                  }
-                : goal,
-            ),
-          })),
-        };
-      };
-
       const context = await optimisticUpdater({
-        queryKey: targetQueryKey,
-        updater,
+        queryKey: feedQueryKey,
+        updater: updateFeedEmojiCount(goalId, emojiId),
       });
       return context;
     },
@@ -73,7 +37,7 @@ export const useCreateEmojiForFeed = () => {
     },
     onError: (_, __, context) => {
       toast.warning('잠시후 다시 시도해주세요.');
-      queryClient.setQueryData(targetQueryKey, context?.previous);
+      queryClient.setQueryData(feedQueryKey, context?.previous);
     },
   });
 };
--- src/hooks/reactQuery/emoji/useDeleteReactedEmojiForFeed.ts
+++ src/hooks/reactQuery/emoji/useDeleteReactedEmojiForFeed.ts
@@ -1,11 +1,10 @@
-import type { InfiniteData } from '@tanstack/react-query';
 import { useMutation } from '@tanstack/react-query';
 
 import { api } from '@/apis';
+import { useUpdateFeedEmojiCount } from '@/features/home/hooks/useUpdateFeedEmojiCount';
 import { useUpdateTimelineEmojiCount } from '@/features/home/hooks/useUpdateTimelineEmojiCount';
 import { useToast } from '@/hooks';
 
-import type { GoalFeedResponse } from '../goal/useGetGoalFeeds';
 import { useOptimisticUpdate } from '../useOptimisticUpdate';
 
 type EmojiRequestParams = {
@@ -17,8 +16,8 @@ export const useDeleteReactedEmojiForFeed = () => {
   const toast = useToast();
 
   const { queryClient, optimisticUpdater } = useOptimisticUpdate();
-  const targetQueryKey = ['goalFeeds'];
 
+  const { queryKey: feedQueryKey, updateFeedEmojiCount } = useUpdateFeedEmojiCount({ isAddingEmoji: false });
   const { queryKey: timelineQueryKey, updateTimelineEmojiCount } = useUpdateTimelineEmojiCount({
     isAddingEmoji: false,
   });
@@ -26,36 +25,11 @@ export const useDeleteReactedEmojiForFeed = () => {
   return useMutation({
     mutationFn: ({ goalId, emojiId }: EmojiRequestParams) => api.delete(`/goal/${goalId}/emoji/${emojiId}`),
     onMutate: async ({ goalId, emojiId }) => {
-      const updater = (old: InfiniteData<GoalFeedResponse>) => {
-        if (!old) return null;
-
-        return {
-          ...old,
-          pages: old.pages.map((page) => ({
-            ...page,
-            goals: page.goals.map((goal) =>
-              goal.goal.id === goalId
-                ? {
-                    ...goal,
-                    emojis: goal.emojis
-                      .map((emoji) =>
-                        emoji.id === emojiId
-                          ? { ...emoji, reactCount: emoji.reactCount - 1, isMyReaction: false }
-                          : emoji,
-                      )
-                      .filter(({ reactCount }) => reactCount > 0),
-                    count: { ...goal.count, reaction: goal.count.reaction - 1 },
-                  }
-                : goal,
-            ),
-          })),
-        };
-      };
-
       const context = await optimisticUpdater({
-        queryKey: targetQueryKey,
-        updater,
+        queryKey: feedQueryKey,
+        updater: updateFeedEmojiCount(goalId, emojiId),
       });
+
       return context;
     },
     onSuccess: (_, { goalId, emojiId }) => {
@@ -63,8 +37,8 @@ export const useDeleteReactedEmojiForFeed = () => {
       queryClient.setQueryData(timelineQueryKey, updateTimelineEmojiCount(goalId, emojiId));
     },
     onError: (_, __, context) => {
-      toast.warning('잠시후 다시 시도해주세요.');
-      queryClient.setQueryData(targetQueryKey, context?.previous);
+      toast.warning('이모지 삭제: 잠시후 다시 시도해주세요.');
+      queryClient.setQueryData(feedQueryKey, context?.previous);
     },
   });
 };
