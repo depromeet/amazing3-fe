77d071ea529212d4800d7439b019c315c5d4607b
minkyung <newminkyung@gmail.com>
2024-04-21 00:45:36 +0900

    fix: 타임라인에서 새로고침 시 지도 페이지로 이동하는 이슈 수정 (#261)
    * feat: 쿼리 스트링 업데이트하는 훅 추가
    
    * fix: 새로고침 시에 탭이 유지될 수 있도록 탭 상태를 쿼리 스트링으로 관리

--- src/features/home/components/homeTab/HomeTab.tsx
+++ src/features/home/components/homeTab/HomeTab.tsx
@@ -1,5 +1,9 @@
 import type { Dispatch, FC, SetStateAction, SVGProps } from 'react';
 import { useState } from 'react';
+import type { Route } from 'next';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+import { useUpdateQueryString } from '@/hooks/useUpdateQueryString';
 
 import { HomeTabMenu } from './HomeTabMenu';
 
@@ -15,11 +19,23 @@ interface HomeTabProps {
 }
 
 export const HomeTab = ({ tabList, onChangeActiveTab }: HomeTabProps) => {
-  const [activeMenu, setActiveMenu] = useState<TabMenu>(tabList[0]);
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const updateQueryString = useUpdateQueryString();
+
+  const currentTabName = searchParams.get('tab') ?? tabList[0].name;
+  const initialActiveTabIndex = tabList.findIndex((tab) => tab.name === currentTabName) || 0;
+
+  const [activeMenu, setActiveMenu] = useState<TabMenu>(tabList[initialActiveTabIndex]);
 
   const handleClickMenu = (tab: TabMenu) => () => {
     onChangeActiveTab(tab);
     setActiveMenu(tab);
+
+    const queryString = updateQueryString('tab', tab.name);
+    router.push(`${pathname}?${queryString}` as Route);
   };
 
   return (
@@ -39,7 +55,11 @@ export const HomeTab = ({ tabList, onChangeActiveTab }: HomeTabProps) => {
           );
         })}
 
-        <span className="absolute w-[24px] h-[24px] top-0 left-0 -z-10 origin-center rounded-sm bg-white transition-all duration-300 peer-checked/tab-1:left-[50%]"></span>
+        <span
+          className={`absolute w-[24px] h-[24px] top-0 left-0 -z-10 origin-center rounded-sm bg-white transition-all duration-300 peer-checked/tab-1:left-[50%] ${
+            initialActiveTabIndex === 0 ? 'left-0' : 'left-[50%]'
+          }`}
+        ></span>
       </div>
     </div>
   );
