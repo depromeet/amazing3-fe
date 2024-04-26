13e4b8f559edecf7a230b673720e16fde134f43d
deepbig <34956359+deepbig@users.noreply.github.com>
2024-04-26 22:56:47 +0900

    username 변경 사항이 없는 경우 체크 로직 무시

--- src/features/my/contexts/UpdateMemberDataFormProvider.tsx
+++ src/features/my/contexts/UpdateMemberDataFormProvider.tsx
@@ -28,15 +28,17 @@ const UpdateMemberDataFormProvider = ({ children }: PropsWithChildren) => {
     if (!nickname || !username) return;
 
     // TODO: react-hook-form을 사용하는 input field는 별도로 component로 분리해서 에러 상태에 대한 처리를 할 수 있도록 수정
-    if (memberData?.username !== username && username.toUpperCase().startsWith('BANDIBOODI-')) {
-      toast.warning('BANDIBOODI-로 시작하는 닉네임은 사용할 수 없습니다.');
-      return;
-    }
+    if (memberData?.username !== username) {
+      if (username.toUpperCase().startsWith('BANDIBOODI-')) {
+        toast.warning('BANDIBOODI-로 시작하는 아이디는 사용할 수 없습니다.');
+        return;
+      }
 
-    // Username may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen.
-    if (!/^[a-zA-Z0-9]+(-?[a-zA-Z0-9])*[a-zA-Z0-9]$/.test(username)) {
-      toast.warning('닉네임은 영문, 숫자, 하이픈(-)만 사용할 수 있고, 하이픈(-)은 처음과 끝에 사용할 수 없습니다.');
-      return;
+      // Username may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen.
+      if (!/^[a-zA-Z0-9]+(-?[a-zA-Z0-9])*[a-zA-Z0-9]$/.test(username)) {
+        toast.warning('아이디는 영문, 숫자, 하이픈(-)만 사용할 수 있고, 하이픈(-)은 처음과 끝에 사용할 수 없습니다.');
+        return;
+      }
     }
 
     // Convert birth from "YYYY.MM.DD" to "YYYY-MM-DD"
