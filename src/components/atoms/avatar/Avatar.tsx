0bb340dbd21bd024fda5a63e46abcdc4dbe8d046
Doeunnkimm <doeunnkimm@gmail.com>
2024-04-30 00:03:47 +0900

    feat: 에러 이미지 대응

--- src/components/atoms/avatar/Avatar.tsx
+++ src/components/atoms/avatar/Avatar.tsx
@@ -1,23 +1,33 @@
-import Image from 'next/image';
+import type { SyntheticEvent } from 'react';
+import Link from 'next/link';
 
 import UserIcon from '@/assets/icons/user.svg';
+import DefaultProfileImage from '@/assets/images/default-profile.png';
 
 interface AvatarProps {
   size?: number;
   profileImage?: string;
+  href?: string;
 }
 
-export const Avatar = ({ size = 24, profileImage }: AvatarProps) => {
+export const Avatar = ({ size = 24, profileImage, href }: AvatarProps) => {
+  const handleAddDefaultImg = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.src = DefaultProfileImage.src;
+  };
+
   return (
-    <div
-      style={{ width: size, height: size }}
-      className="flex justify-center items-center rounded-xl overflow-hidden bg-white"
-    >
-      {!profileImage ? (
-        <UserIcon width={`calc(${size} / 1.5)`} height={`calc(${size} / 1.5)`} />
-      ) : (
-        <Image src={profileImage} width={size} height={size} alt="profile_image" />
-      )}
-    </div>
+    <Link href={{ pathname: href }}>
+      <div
+        style={{ width: size, height: size }}
+        className="flex justify-center items-center rounded-xl overflow-hidden bg-white"
+      >
+        {!profileImage ? (
+          <UserIcon width={`calc(${size} / 1.5)`} height={`calc(${size} / 1.5)`} />
+        ) : (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img src={profileImage} width={size} height={size} alt="profile_image" onError={handleAddDefaultImg} />
+        )}
+      </div>
+    </Link>
   );
 };
