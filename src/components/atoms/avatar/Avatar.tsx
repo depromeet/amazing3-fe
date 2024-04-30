0df87b0187bb9ab89c2d029db7171bb9a91de705
Doeun Kim <doeunnkimm@gmail.com>
2024-04-30 22:33:11 +0900

    fix: 이모지 바텀시트에서 프로필 이미지가 길어지는 문제 해결 (#283)
    * fix: 이모지 바텀시트에서 Avatar로 적용
    
    * chore: Avatar에 href 제거

--- src/components/atoms/avatar/Avatar.tsx
+++ src/components/atoms/avatar/Avatar.tsx
@@ -1,5 +1,4 @@
 import type { SyntheticEvent } from 'react';
-import Link from 'next/link';
 
 import UserIcon from '@/assets/icons/user.svg';
 import DefaultProfileImage from '@/assets/images/default-profile.png';
@@ -7,27 +6,24 @@ import DefaultProfileImage from '@/assets/images/default-profile.png';
 interface AvatarProps {
   size?: number;
   profileImage?: string;
-  href?: string;
 }
 
-export const Avatar = ({ size = 24, profileImage, href }: AvatarProps) => {
+export const Avatar = ({ size = 24, profileImage }: AvatarProps) => {
   const handleAddDefaultImg = (e: SyntheticEvent<HTMLImageElement, Event>) => {
     e.currentTarget.src = DefaultProfileImage.src;
   };
 
   return (
-    <Link href={{ pathname: href }}>
-      <div
-        style={{ width: size, height: size }}
-        className="flex justify-center items-center rounded-xl overflow-hidden bg-white"
-      >
-        {!profileImage ? (
-          <UserIcon width={`calc(${size} / 1.5)`} height={`calc(${size} / 1.5)`} />
-        ) : (
-          // eslint-disable-next-line @next/next/no-img-element
-          <img src={profileImage} width={size} height={size} alt="profile_image" onError={handleAddDefaultImg} />
-        )}
-      </div>
-    </Link>
+    <div
+      style={{ width: size, height: size }}
+      className="flex justify-center items-center rounded-xl overflow-hidden bg-white"
+    >
+      {!profileImage ? (
+        <UserIcon width={`calc(${size} / 1.5)`} height={`calc(${size} / 1.5)`} />
+      ) : (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img src={profileImage} width={size} height={size} alt="profile_image" onError={handleAddDefaultImg} />
+      )}
+    </div>
   );
 };
